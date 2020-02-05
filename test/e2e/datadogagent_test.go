0f3c72cab944593d8659d04e4f4c71f21e0dc187
Ivan Ilichev <ivan.ilichev@datadoghq.com>
2020-02-05 18:41:44 -0500

    Up timeout to 5min

--- test/e2e/datadogagent_test.go
+++ test/e2e/datadogagent_test.go
@@ -34,7 +34,7 @@ import (
 
 var (
 	retryInterval        = time.Second * 5
-	timeout              = time.Second * 120
+	timeout              = time.Minute * 5
 	cleanupRetryInterval = time.Second * 1
 	cleanupTimeout       = time.Second * 60
 
