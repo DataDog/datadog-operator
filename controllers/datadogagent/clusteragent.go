870fbeddee5ff8ae6553e84d6e16f5fbd03c6b21
Vincent Boulineau <58430298+vboulineau@users.noreply.github.com>
2020-11-05 16:01:09 +0100

    Fix compliance CheckInterval set in DCA/SecAgent (#169)

--- controllers/datadogagent/clusteragent.go
+++ controllers/datadogagent/clusteragent.go
@@ -508,7 +508,7 @@ func getEnvVarsForClusterAgent(dda *datadoghqv1alpha1.DatadogAgent) []corev1.Env
 		if dda.Spec.Agent.Security.Compliance.CheckInterval != nil {
 			envVars = append(envVars, corev1.EnvVar{
 				Name:  datadoghqv1alpha1.DDComplianceConfigCheckInterval,
-				Value: dda.Spec.Agent.Security.Compliance.CheckInterval.String(),
+				Value: strconv.FormatInt(dda.Spec.Agent.Security.Compliance.CheckInterval.Nanoseconds(), 10),
 			})
 		}
 
