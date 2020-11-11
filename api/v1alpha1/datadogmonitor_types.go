f01fb00ba5dadd7b7d2d43f15626ead7e154af2e
celene <celene@datadoghq.com>
2020-11-11 15:20:41 -0500

    add proper scaffolding

--- api/v1alpha1/datadogmonitor_types.go
+++ api/v1alpha1/datadogmonitor_types.go
@@ -11,21 +11,21 @@ import (
 )
 
 // DatadogMonitorSpec defines the desired state of DatadogMonitor
-type DatadogMonitorSpec struct {	
+type DatadogMonitorSpec struct {
 	// Query is the Datadog query
 	Query string `json:"query,omitempty"`
 	// Type is the monitor type
 	Type DatadogMonitorType `json:"type,omitempty"`
-	// Title is the monitor title 
+	// Title is the monitor title
 	Title string `json:"title,omitempty"`
 	// Message is the message to include in a monitor notification
 	Message string `json:"message,omitempty"`
 	// Tags is the monitor tags used to organize monitors
 	Tags []string `json:"tags,omitempty"`
-	
+
 	// Options are the optional parameters of a monitor
 	Options DatadogMonitorOptions `json:"options,omitempty"`
-	}
+}
 
 // DatadogMonitorType defines the type of monitor
 type DatadogMonitorType string
@@ -37,25 +37,25 @@ const (
 
 // DatadogMonitorOptions define the optional parameters of a monitor
 type DatadogMonitorOptions struct {
-	// Time (in seconds) to delay evaluation, as a non-negative integer. For example, if the value is set to 300 (5min), 
-	// the timeframe is set to last_5m and the time is 7:00, the monitor evaluates data from 6:50 to 6:55. 
+	// Time (in seconds) to delay evaluation, as a non-negative integer. For example, if the value is set to 300 (5min),
+	// the timeframe is set to last_5m and the time is 7:00, the monitor evaluates data from 6:50 to 6:55.
 	// This is useful for AWS CloudWatch and other backfilled metrics to ensure the monitor always has data during evaluation.
 	EvaluationDelay int `json:"evaluationDelay,omitempty"`
 	// Whether or not the monitor is locked (only editable by creator and admins).
 	Locked bool `json:"locked,omitempty"`
-	// Time (in seconds) to allow a host to boot and applications to fully start before starting the evaluation of 
+	// Time (in seconds) to allow a host to boot and applications to fully start before starting the evaluation of
 	// monitor results. Should be a non negative integer.
 	NewHostDelay int `json:"newHostDelay,omitempty"`
-	// The number of minutes before a monitor notifies after data stops reporting. Datadog recommends at least 2x the 
-	// monitor timeframe for metric alerts or 2 minutes for service checks. If omitted, 2x the evaluation timeframe 
+	// The number of minutes before a monitor notifies after data stops reporting. Datadog recommends at least 2x the
+	// monitor timeframe for metric alerts or 2 minutes for service checks. If omitted, 2x the evaluation timeframe
 	// is used for metric alerts, and 24 hours is used for service checks.
 	NoDataTimeframe int `json:"noDataTimeframe,omitempty"`
 	// A Boolean indicating whether this monitor notifies when data stops reporting.
 	NotifyNoData bool `json:"notifyNoData,omitempty"`
-	// The number of minutes after the last notification before a monitor re-notifies on the current status. 
+	// The number of minutes after the last notification before a monitor re-notifies on the current status.
 	// It only re-notifies if it’s not resolved.
 	RenotifyInterval int `json:"renotifyInterval,omitempty"`
-	// A Boolean indicating whether this monitor needs a full window of data before it’s evaluated. We highly 
+	// A Boolean indicating whether this monitor needs a full window of data before it’s evaluated. We highly
 	// recommend you set this to false for sparse metrics, otherwise some evaluations are skipped. Default is false.
 	RequireFullWindow bool `json:"requireFullWindow,omitempty"`
 }
@@ -68,14 +68,14 @@ type DatadogMonitorStatus struct {
 	Conditions []DatadogMonitorCondition `json:"conditions,omitempty"`
 
 	// ID is the monitor ID generated in Datadog
-	ID                 int `json:"id,omitempty"`
+	ID int `json:"id,omitempty"`
 	// MonitorState is the overall state of monitor
-  	MonitorState       DatadogMonitorState `json:"monitorState,omitempty"`
-  	// TriggeredState only includes details for monitor groups that are triggering
-  	TriggeredState     []DatadogMonitorTriggeredState `json:"triggeredState,omitempty"` 
-    // DowntimeStatus defines whether the monitor is downtimed
-  	DowntimeStatus     DatadogMonitorDowntimeStatus `json:"downtimeStatus,omitempty"`
-    // Creator is the identify of the monitor creator
+	MonitorState DatadogMonitorState `json:"monitorState,omitempty"`
+	// TriggeredState only includes details for monitor groups that are triggering
+	TriggeredState []DatadogMonitorTriggeredState `json:"triggeredState,omitempty"`
+	// DowntimeStatus defines whether the monitor is downtimed
+	DowntimeStatus DatadogMonitorDowntimeStatus `json:"downtimeStatus,omitempty"`
+	// Creator is the identify of the monitor creator
 	Creator string `json:"creator,omitempty"`
 	// Created is the time the monitor was created
 	Created metav1.Time `json:"created,omitempty"`
@@ -124,7 +124,7 @@ const (
 	DatadogMonitorStateOK DatadogMonitorState = "OK"
 	// DatadogMonitorStateAlert means the DatadogMonitor triggered an alert
 	DatadogMonitorStateAlert DatadogMonitorState = "Alert"
-	// DatadogMonitorStateWarn means the DatadogMonitor triggered a warning 
+	// DatadogMonitorStateWarn means the DatadogMonitor triggered a warning
 	DatadogMonitorStateWarn DatadogMonitorState = "Warn"
 	// DatadogMonitorStateNoData means the DatadogMonitor triggered a no data alert
 	DatadogMonitorStateNoData DatadogMonitorState = "No Data"
@@ -139,16 +139,16 @@ const (
 // DatadogMonitorTriggeredState represents the details of a triggering DatadogMonitor
 // The DatadogMonitor is triggering if one of its groups is in Alert, Warn, or No Data
 type DatadogMonitorTriggeredState struct {
-    // MonitorGroup is the name of the triggering group
-    MonitorGroup          string `json:"monitorGroup,omitempty"`
-    State                 DatadogMonitorState `json:"state,omitempty"`
-    LastTransitionTime    metav1.Time `json:"lastTransitionTime,omitempty"`
+	// MonitorGroup is the name of the triggering group
+	MonitorGroup       string              `json:"monitorGroup,omitempty"`
+	State              DatadogMonitorState `json:"state,omitempty"`
+	LastTransitionTime metav1.Time         `json:"lastTransitionTime,omitempty"`
 }
 
 // DatadogMonitorDowntimeStatus represents the downtime status of a DatadogMonitor
 type DatadogMonitorDowntimeStatus struct {
-  IsDowntimed     bool `json:"isDowntimed,omitempty"`
-  DowntimeID      int `json:"downtimeId,omitempty"`
+	IsDowntimed bool `json:"isDowntimed,omitempty"`
+	DowntimeID  int  `json:"downtimeId,omitempty"`
 }
 
 // DatadogMonitor is the Schema for the datadogmonitor API
@@ -176,7 +176,6 @@ type DatadogMonitorList struct {
 	metav1.ListMeta `json:"metadata,omitempty"`
 	Items           []DatadogMonitor `json:"items"`
 }
-
 
 func init() {
 	SchemeBuilder.Register(&DatadogMonitor{}, &DatadogMonitorList{})
