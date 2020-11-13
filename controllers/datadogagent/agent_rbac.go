983d0b904fc4edfbd7f5d77b2bba55543804f536
Charly Fontaine <charly@datadoghq.com>
2020-11-13 09:03:53 -0500

    Update controllers/datadogagent/agent_rbac.go
    Co-authored-by: Cedric Lamoriniere <cedric.lamoriniere@datadoghq.com>

--- controllers/datadogagent/agent_rbac.go
+++ controllers/datadogagent/agent_rbac.go
@@ -65,11 +65,7 @@ func (r *Reconciler) manageAgentRBACs(logger logr.Logger, dda *datadoghqv1alpha1
 		return reconcile.Result{}, err
 	}
 
-	if result, err := r.udpateIfNeededAgentClusterRoleBinding(logger, dda, rbacResourcesName, serviceAccountName, agentVersion, clusterRoleBinding); err != nil {
-		return result, err
-	}
-
-	return reconcile.Result{}, nil
+	return r.udpateIfNeededAgentClusterRoleBinding(logger, dda, rbacResourcesName, serviceAccountName, agentVersion, clusterRoleBinding)
 }
 
 // cleanupAgentRbacResources deletes ClusterRole, ClusterRoleBindings, and ServiceAccount of the Agent
