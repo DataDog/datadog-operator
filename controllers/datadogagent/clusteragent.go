35d3f353103fef6b91cf3547a47e0099de1a426e
Lénaïc Huard <L3n41c@users.noreply.github.com>
2020-12-14 12:50:41 +0100

    Fix issues with Secret management (#190)

--- controllers/datadogagent/clusteragent.go
+++ controllers/datadogagent/clusteragent.go
@@ -580,12 +580,10 @@ func getEnvVarsForClusterAgent(dda *datadoghqv1alpha1.DatadogAgent) ([]corev1.En
 		})
 	}
 
-	if needAgentSecret(dda) {
-		envVars = append(envVars, corev1.EnvVar{
-			Name:      datadoghqv1alpha1.DDAPIKey,
-			ValueFrom: getAPIKeyFromSecret(dda),
-		})
-	}
+	envVars = append(envVars, corev1.EnvVar{
+		Name:      datadoghqv1alpha1.DDAPIKey,
+		ValueFrom: getAPIKeyFromSecret(dda),
+	})
 
 	if spec.Site != "" {
 		envVars = append(envVars, corev1.EnvVar{
