83ee91de731ca9b59a0c1ff2e6f1a5ac36d7f591
Vincent Boulineau <vincent.boulineau@datadoghq.com>
2021-11-16 16:52:20 +0100

    Add documentation on how to test operator bundles

--- hack/patch-crds.sh
+++ hack/patch-crds.sh
@@ -8,6 +8,7 @@ ROOT_DIR=$(git rev-parse --show-toplevel)
 YQ="$ROOT_DIR/bin/yq"
 
 v1beta1=config/crd/bases/v1beta1
+v1=config/crd/bases/v1
 
 # Remove "x-kubernetes-*" as only supported in Kubernetes 1.16+.
 # Users of Kubernetes < 1.16 need to use v1beta1, others need to use v1
@@ -21,3 +22,7 @@ do
     $YQ d -i "$ROOT_DIR/$v1beta1/$crd" $path
   done
 done
+
+# Remove defaultOverride section in DatadogAgent status due to the error: "datadoghq.com_datadogagents.yaml bigger than total allowed limit"
+$YQ d -i "$ROOT_DIR/$v1beta1/datadoghq.com_datadogagents.yaml" 'spec.validation.openAPIV3Schema.properties.status.properties.defaultOverride'
+$YQ d -i "$ROOT_DIR/$v1/datadoghq.com_datadogagents.yaml" 'spec.versions[*].schema.openAPIV3Schema.properties.status.properties.defaultOverride.properties'
