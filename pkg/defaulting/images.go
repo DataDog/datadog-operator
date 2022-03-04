5c12cb76f8a8dde1810142a9063ee4dc2f1d2275
Cedric Lamoriniere <cedric.lamoriniere@datadoghq.com>
2022-03-04 20:56:51 +0100

    update Agent to 7.34.0 and DCA to 1.18.0 (#452)

--- pkg/defaulting/images.go
+++ pkg/defaulting/images.go
@@ -16,9 +16,9 @@ type ContainerRegistry string
 
 const (
 	// AgentLatestVersion correspond to the latest stable agent release
-	AgentLatestVersion = "7.33.0"
+	AgentLatestVersion = "7.34.0"
 	// ClusterAgentLatestVersion correspond to the latest stable cluster-agent release
-	ClusterAgentLatestVersion = "1.17.0"
+	ClusterAgentLatestVersion = "1.18.0"
 
 	// GCRContainerRegistry correspond to the datadoghq GCR registry
 	GCRContainerRegistry ContainerRegistry = "gcr.io/datadoghq"
