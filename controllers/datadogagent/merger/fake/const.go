f742a9e4dfaf41a716a662ee30a1f3d8f173951a
cedric lamoriniere <cedric.lamoriniere@datadoghq.com>
2022-05-02 11:39:48 +0200

    Add unit-test fwk for Feature

---
+++ controllers/datadogagent/merger/fake/const.go
@@ -0,0 +1,8 @@
+package fake
+
+import common "github.com/DataDog/datadog-operator/controllers/datadogagent/common"
+
+const (
+	// AllContainers all containers container name
+	AllContainers common.ContainerName = "all"
+)
