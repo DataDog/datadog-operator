d8ee6903eb808d6067b1f42d6e038ce53d452f2e
David Ortiz <david.ortiz@datadoghq.com>
2022-07-06 11:55:48 +0200

    [v2alpha1] Don't skip

--- apis/datadoghq/v2alpha1/datadogagent_types.go
+++ apis/datadoghq/v2alpha1/datadogagent_types.go
@@ -3,8 +3,6 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
-// nolint
-// +kubebuilder:skip
 package v2alpha1
 
 import (
