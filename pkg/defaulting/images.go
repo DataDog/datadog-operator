d9e8995855a809fcc2adbd5b5850cc472ade6453
David Ortiz <david.ortiz@datadoghq.com>
2022-08-03 11:12:33 +0200

    [defaulting/images] Update agent to 7.38.1

--- pkg/defaulting/images.go
+++ pkg/defaulting/images.go
@@ -16,7 +16,7 @@ type ContainerRegistry string
 
 const (
 	// AgentLatestVersion correspond to the latest stable agent release
-	AgentLatestVersion = "7.38.0"
+	AgentLatestVersion = "7.38.1"
 	// ClusterAgentLatestVersion correspond to the latest stable cluster-agent release
 	ClusterAgentLatestVersion = "1.22.0"
 
