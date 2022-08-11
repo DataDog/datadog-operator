b3d4ea45f64bf4a8a8c71d6ecfe5c353d2dd9c3f
khewonc <39867936+khewonc@users.noreply.github.com>
2022-08-11 17:36:14 -0400

    Add EDS Support (#591)
    * Add EDS Support

--- controllers/datadogagent/controller_reconcile_v2_common.go
+++ controllers/datadogagent/controller_reconcile_v2_common.go
@@ -21,10 +21,13 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	edsv1alpha1 "github.com/DataDog/extendeddaemonset/api/v1alpha1"
 )
 
 type updateDepStatusComponentFunc func(deployment *appsv1.Deployment, newStatus *datadoghqv2alpha1.DatadogAgentStatus, updateTime metav1.Time, status metav1.ConditionStatus, reason, message string)
 type updateDSStatusComponentFunc func(daemonset *appsv1.DaemonSet, newStatus *datadoghqv2alpha1.DatadogAgentStatus, updateTime metav1.Time, status metav1.ConditionStatus, reason, message string)
+type updateEDSStatusComponentFunc func(eds *edsv1alpha1.ExtendedDaemonSet, newStatus *datadoghqv2alpha1.DatadogAgentStatus, updateTime metav1.Time, status metav1.ConditionStatus, reason, message string)
 
 func (r *Reconciler) createOrUpdateDeployment(parentLogger logr.Logger, dda *datadoghqv2alpha1.DatadogAgent, deployment *appsv1.Deployment, newStatus *datadoghqv2alpha1.DatadogAgentStatus, updateStatusFunc updateDepStatusComponentFunc) (reconcile.Result, error) {
 	logger := parentLogger.WithValues("deployment.Namespace", deployment.Namespace, "deployment.Name", deployment.Name)
@@ -32,12 +35,12 @@ func (r *Reconciler) createOrUpdateDeployment(parentLogger logr.Logger, dda *dat
 	var result reconcile.Result
 	var err error
 
-	// Set DatadogAgent instance  instance as the owner and controller
+	// Set DatadogAgent instance as the owner and controller
 	if err = controllerutil.SetControllerReference(dda, deployment, r.scheme); err != nil {
 		return reconcile.Result{}, err
 	}
 
-	// From here the PodTemplateSpec should be ready, we can generate the hash that will be use to compare this deployment with the current (if exist).
+	// From here the PodTemplateSpec should be ready, we can generate the hash that will be used to compare this deployment with the current one (if it exists).
 	var hash string
 	hash, err = comparison.SetMD5DatadogAgentGenerationAnnotation(&deployment.ObjectMeta, deployment.Spec)
 	if err != nil {
@@ -51,23 +54,23 @@ func (r *Reconciler) createOrUpdateDeployment(parentLogger logr.Logger, dda *dat
 	}
 
 	currentDeployment := &appsv1.Deployment{}
-	alreadyExist := true
+	alreadyExists := true
 	err = r.client.Get(context.TODO(), nsName, currentDeployment)
 	if err != nil {
 		if apierrors.IsNotFound(err) {
 			logger.Info("deployment is not found")
-			alreadyExist = false
+			alreadyExists = false
 		} else {
 			logger.Error(err, "unexpected error during deployment get")
 			return reconcile.Result{}, err
 		}
 	}
 
-	if alreadyExist {
+	if alreadyExists {
 		// check if same hash
 		needUpdate := !comparison.IsSameSpecMD5Hash(hash, currentDeployment.GetAnnotations())
 		if !needUpdate {
-			// no need to update to stop here the process
+			// no need to update hasn't changed
 			now := metav1.NewTime(time.Now())
 			updateStatusFunc(currentDeployment, newStatus, now, metav1.ConditionTrue, "deployment_up_to_date", "Deployment up-to-date")
 			return reconcile.Result{}, nil
@@ -75,7 +78,7 @@ func (r *Reconciler) createOrUpdateDeployment(parentLogger logr.Logger, dda *dat
 
 		logger.Info("Updating Deployment")
 
-		// TODO: these parameter can be added to the override.PodTemplateSpec. (it exist in v1alpha)
+		// TODO: these parameters can be added to the override.PodTemplateSpec. (It exists in v1alpha1)
 		keepAnnotationsFilter := ""
 		keepLabelsFilter := ""
 
@@ -119,12 +122,12 @@ func (r *Reconciler) createOrUpdateDaemonset(parentLogger logr.Logger, dda *data
 	var result reconcile.Result
 	var err error
 
-	// Set DatadogAgent instance  instance as the owner and controller
+	// Set DatadogAgent instance as the owner and controller
 	if err = controllerutil.SetControllerReference(dda, daemonset, r.scheme); err != nil {
 		return reconcile.Result{}, err
 	}
 
-	// From here the PodTemplateSpec should be ready, we can generate the hash that will be use to compare this daemonset with the current (if exist).
+	// From here the PodTemplateSpec should be ready, we can generate the hash that will be used to compare this daemonset with the current one (if it exists).
 	var hash string
 	hash, err = comparison.SetMD5DatadogAgentGenerationAnnotation(&daemonset.ObjectMeta, daemonset.Spec)
 	if err != nil {
@@ -138,35 +141,35 @@ func (r *Reconciler) createOrUpdateDaemonset(parentLogger logr.Logger, dda *data
 	}
 
 	currentDaemonset := &appsv1.DaemonSet{}
-	alreadyExist := true
+	alreadyExists := true
 	err = r.client.Get(context.TODO(), nsName, currentDaemonset)
 	if err != nil {
 		if apierrors.IsNotFound(err) {
 			logger.Info("daemonset is not found")
-			alreadyExist = false
+			alreadyExists = false
 		} else {
 			logger.Error(err, "unexpected error during daemonset get")
 			return reconcile.Result{}, err
 		}
 	}
 
-	if alreadyExist {
+	if alreadyExists {
 		// check if same hash
 		needUpdate := !comparison.IsSameSpecMD5Hash(hash, currentDaemonset.GetAnnotations())
 		if !needUpdate {
-			// Even if the DaemonSet is still the same, it's status might have
+			// Even if the DaemonSet is still the same, its status might have
 			// changed (for example, the number of pods ready). This call is
 			// needed to keep the agent status updated.
 			now := metav1.NewTime(time.Now())
 			newStatus.Agent = datadoghqv2alpha1.UpdateDaemonSetStatus(currentDaemonset, newStatus.Agent, &now)
 
-			// no need to update the DaemonSet to stop here the process
+			// Stop reconcile loop since DaemonSet hasn't changed
 			return reconcile.Result{}, nil
 		}
 
 		logger.Info("Updating Daemonset")
 
-		// TODO: these parameter can be added to the override.PodTemplateSpec. (it exist in v1alpha)
+		// TODO: these parameters can be added to the override.PodTemplateSpec. (It exists in v1alpha1)
 		keepAnnotationsFilter := ""
 		keepLabelsFilter := ""
 
@@ -181,7 +184,7 @@ func (r *Reconciler) createOrUpdateDaemonset(parentLogger logr.Logger, dda *data
 		if err != nil {
 			return reconcile.Result{}, err
 		}
-		event := buildEventInfo(updateDaemonset.Name, updateDaemonset.Namespace, deploymentKind, datadog.UpdateEvent)
+		event := buildEventInfo(updateDaemonset.Name, updateDaemonset.Namespace, daemonSetKind, datadog.UpdateEvent)
 		r.recordEvent(dda, event)
 		updateStatusFunc(updateDaemonset, newStatus, now, metav1.ConditionTrue, "Daemonset_updated", "Daemonset updated")
 	} else {
@@ -198,6 +201,95 @@ func (r *Reconciler) createOrUpdateDaemonset(parentLogger logr.Logger, dda *data
 	}
 
 	logger.Info("Creating Daemonset")
+
+	return result, err
+}
+
+func (r *Reconciler) createOrUpdateExtendedDaemonset(parentLogger logr.Logger, dda *datadoghqv2alpha1.DatadogAgent, eds *edsv1alpha1.ExtendedDaemonSet, newStatus *datadoghqv2alpha1.DatadogAgentStatus, updateStatusFunc updateEDSStatusComponentFunc) (reconcile.Result, error) {
+	logger := parentLogger.WithValues("ExtendedDaemonSet.Namespace", eds.Namespace, "ExtendedDaemonSet.Name", eds.Name)
+
+	var result reconcile.Result
+	var err error
+
+	// Set DatadogAgent instance as the owner and controller
+	if err = controllerutil.SetControllerReference(dda, eds, r.scheme); err != nil {
+		return reconcile.Result{}, err
+	}
+
+	// From here the PodTemplateSpec should be ready, we can generate the hash that will be used to compare this extendeddaemonset with the current one (if it exists).
+	var hash string
+	hash, err = comparison.SetMD5DatadogAgentGenerationAnnotation(&eds.ObjectMeta, eds.Spec)
+	if err != nil {
+		return result, err
+	}
+
+	// Get the current extendeddaemonset and compare
+	nsName := types.NamespacedName{
+		Name:      eds.GetName(),
+		Namespace: eds.GetNamespace(),
+	}
+
+	currentEDS := &edsv1alpha1.ExtendedDaemonSet{}
+	alreadyExists := true
+	err = r.client.Get(context.TODO(), nsName, currentEDS)
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			logger.Info("ExtendedDaemonSet is not found")
+			alreadyExists = false
+		} else {
+			logger.Error(err, "unexpected error during ExtendedDaemonSet get")
+			return reconcile.Result{}, err
+		}
+	}
+
+	if alreadyExists {
+		// check if same hash
+		needUpdate := !comparison.IsSameSpecMD5Hash(hash, currentEDS.GetAnnotations())
+		if !needUpdate {
+			// Even if the EDS is still the same, its status might have
+			// changed (for example, the number of pods ready). This call is
+			// needed to keep the agent status updated.
+			now := metav1.NewTime(time.Now())
+			newStatus.Agent = datadoghqv2alpha1.UpdateExtendedDaemonSetStatus(currentEDS, newStatus.Agent, &now)
+
+			// Stop reconcile loop since EDS hasn't changed
+			return reconcile.Result{}, nil
+		}
+
+		logger.Info("Updating ExtendedDaemonSet")
+
+		// TODO: these parameters can be added to the override.PodTemplateSpec. (It exists in v1alpha1)
+		keepAnnotationsFilter := ""
+		keepLabelsFilter := ""
+
+		// Copy possibly changed fields
+		updateEDS := eds.DeepCopy()
+		updateEDS.Spec = *eds.Spec.DeepCopy()
+		updateEDS.Annotations = mergeAnnotationsLabels(logger, currentEDS.GetAnnotations(), eds.GetAnnotations(), keepAnnotationsFilter)
+		updateEDS.Labels = mergeAnnotationsLabels(logger, currentEDS.GetLabels(), eds.GetLabels(), keepLabelsFilter)
+
+		now := metav1.NewTime(time.Now())
+		err = kubernetes.UpdateFromObject(context.TODO(), r.client, updateEDS, currentEDS.ObjectMeta)
+		if err != nil {
+			return reconcile.Result{}, err
+		}
+		event := buildEventInfo(updateEDS.Name, updateEDS.Namespace, extendedDaemonSetKind, datadog.UpdateEvent)
+		r.recordEvent(dda, event)
+		updateStatusFunc(updateEDS, newStatus, now, metav1.ConditionTrue, "ExtendedDaemonSet_updated", "ExtendedDaemonSet updated")
+	} else {
+		now := metav1.NewTime(time.Now())
+
+		err = r.client.Create(context.TODO(), eds)
+		if err != nil {
+			updateStatusFunc(nil, newStatus, now, metav1.ConditionFalse, "create_failed", "Unable to create ExtendedDaemonSet")
+			return reconcile.Result{}, err
+		}
+		event := buildEventInfo(eds.Name, eds.Namespace, extendedDaemonSetKind, datadog.CreationEvent)
+		r.recordEvent(dda, event)
+		updateStatusFunc(eds, newStatus, now, metav1.ConditionTrue, "create_success", "ExtendedDaemonSet created")
+	}
+
+	logger.Info("Creating ExtendedDaemonSet")
 
 	return result, err
 }
