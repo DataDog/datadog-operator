44425246020c9a09293e879f54c26a06158f8557
Charly Fontaine <charly@datadoghq.com>
2022-10-28 14:09:52 -0400

    Adding support to override service accounts for components (#634)

--- apis/datadoghq/v2alpha1/utils_test.go
+++ apis/datadoghq/v2alpha1/utils_test.go
@@ -6,11 +6,14 @@
 package v2alpha1
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 
+	"github.com/DataDog/datadog-operator/apis/datadoghq/common"
 	commonv1 "github.com/DataDog/datadog-operator/apis/datadoghq/common/v1"
 	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func TestConvertCustomConfig(t *testing.T) {
@@ -66,6 +69,53 @@ func TestConvertCustomConfig(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := ConvertCustomConfig(tt.config); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("ConvertCustomConfig() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestServiceAccountOverride(t *testing.T) {
+	customServiceAccount := "fake"
+	ddaName := "test-dda"
+	tests := []struct {
+		name string
+		dda  *DatadogAgent
+		want map[ComponentName]string
+	}{
+		{
+			name: "custom serviceaccount for dca and clc",
+			dda: &DatadogAgent{
+				ObjectMeta: v1.ObjectMeta{
+					Name: ddaName,
+				},
+				Spec: DatadogAgentSpec{
+					Override: map[ComponentName]*DatadogAgentComponentOverride{
+						ClusterAgentComponentName: &DatadogAgentComponentOverride{
+							ServiceAccountName: &customServiceAccount,
+						},
+						ClusterChecksRunnerComponentName: &DatadogAgentComponentOverride{
+							ServiceAccountName: &customServiceAccount,
+						},
+					},
+				},
+			},
+			want: map[ComponentName]string{
+				ClusterAgentComponentName:        customServiceAccount,
+				NodeAgentComponentName:           fmt.Sprintf("%s-%s", ddaName, common.DefaultAgentResourceSuffix),
+				ClusterChecksRunnerComponentName: customServiceAccount,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			res := map[ComponentName]string{}
+			res[NodeAgentComponentName] = GetAgentServiceAccount(tt.dda)
+			res[ClusterChecksRunnerComponentName] = GetClusterChecksRunnerServiceAccount(tt.dda)
+			res[ClusterAgentComponentName] = GetClusterAgentServiceAccount(tt.dda)
+			for name, sa := range tt.want {
+				if res[name] != sa {
+					t.Errorf("Service Account Override error = %v, want %v", res[name], tt.want[name])
+				}
 			}
 		})
 	}
