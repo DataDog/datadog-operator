3e49b01c83893df10ca384a10520e600f0eeb301
David Ortiz <david.ortiz@datadoghq.com>
2022-11-02 18:14:04 +0100

    [defaulting/images] Update agents to 7.40.0

--- pkg/defaulting/images.go
+++ pkg/defaulting/images.go
@@ -16,9 +16,9 @@ type ContainerRegistry string
 
 const (
 	// AgentLatestVersion correspond to the latest stable agent release
-	AgentLatestVersion = "7.39.2"
+	AgentLatestVersion = "7.40.0"
 	// ClusterAgentLatestVersion correspond to the latest stable cluster-agent release
-	ClusterAgentLatestVersion = "7.39.2"
+	ClusterAgentLatestVersion = "7.40.0"
 
 	// GCRContainerRegistry correspond to the datadoghq GCR registry
 	GCRContainerRegistry ContainerRegistry = "gcr.io/datadoghq"
