6c3a10f2ea17edec58f0b4e759b357d32c933161
khewonc <39867936+khewonc@users.noreply.github.com>
2023-03-16 14:46:55 -0400

    Add md5 hash logging and fix glob pattern (#723)

--- controllers/datadogagent/override/podtemplatespec.go
+++ controllers/datadogagent/override/podtemplatespec.go
@@ -66,6 +66,8 @@ func PodTemplateSpec(logger logr.Logger, manager feature.PodTemplateManagers, ov
 		hash, err := comparison.GenerateMD5ForSpec(override.ExtraConfd)
 		if err != nil {
 			logger.Error(err, "couldn't generate hash for extra confd custom config")
+		} else {
+			logger.V(2).Info("built extra confd from custom config", "hash", hash)
 		}
 		annotationKey := object.GetChecksumAnnotationKey(cmName)
 		manager.Annotation().AddAnnotation(annotationKey, hash)
@@ -80,7 +82,9 @@ func PodTemplateSpec(logger logr.Logger, manager feature.PodTemplateManagers, ov
 		// Add md5 hash annotation for custom config
 		hash, err := comparison.GenerateMD5ForSpec(override.ExtraChecksd)
 		if err != nil {
-			logger.Error(err, "couldn't generate hash for extra checks custom config")
+			logger.Error(err, "couldn't generate hash for extra checksd custom config")
+		} else {
+			logger.V(2).Info("built extra checksd from custom config", "hash", hash)
 		}
 		annotationKey := object.GetChecksumAnnotationKey(cmName)
 		manager.Annotation().AddAnnotation(annotationKey, hash)
@@ -172,6 +176,8 @@ func overrideCustomConfigVolumes(logger logr.Logger, manager feature.PodTemplate
 		hash, err := comparison.GenerateMD5ForSpec(customConfig)
 		if err != nil {
 			logger.Error(err, "couldn't generate hash for custom config", "filename", fileName)
+		} else {
+			logger.V(2).Info("built file from custom config", "filename", fileName, "hash", hash)
 		}
 		annotationKey := object.GetChecksumAnnotationKey(string(fileName))
 		manager.Annotation().AddAnnotation(annotationKey, hash)
