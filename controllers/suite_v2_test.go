8f8b014cc0733f0b2aaa15d4cedde3b2e00c8fd0
Levan Machablishvili <levan.machablishvili@datadoghq.com>
2023-04-13 14:04:30 -0400

    Remove envtest/printer package dependency

--- controllers/suite_v2_test.go
+++ controllers/suite_v2_test.go
@@ -24,10 +24,12 @@ import (
 	"k8s.io/client-go/rest"
 	apiregistrationv1 "k8s.io/kube-aggregator/pkg/apis/apiregistration/v1"
 
+	gc "github.com/onsi/ginkgo/config"
+	"github.com/onsi/ginkgo/reporters"
+	st "github.com/onsi/ginkgo/reporters/stenographer"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
-	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
@@ -50,9 +52,11 @@ var (
 func TestAPIs(t *testing.T) {
 	RegisterFailHandler(Fail)
 
+	stenographer := st.NewFakeStenographer()
+	reporterConfig := gc.DefaultReporterConfigType{}
 	RunSpecsWithDefaultAndCustomReporters(t,
 		"Controller Suite",
-		[]Reporter{printer.NewlineReporter{}})
+		[]Reporter{reporters.NewDefaultReporter(reporterConfig, stenographer)})
 }
 
 var _ = BeforeSuite(func() {
