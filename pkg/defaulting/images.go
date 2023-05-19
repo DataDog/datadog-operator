b4ba06ff6cacceadc4d2bbb0391264c9421c4d7a
Cedric Lamoriniere <cedric.lamoriniere@datadoghq.com>
2023-05-19 13:43:05 +0200

    Default Agent/Cluster-Agent version to 7.44.1

--- pkg/defaulting/images.go
+++ pkg/defaulting/images.go
@@ -16,9 +16,9 @@ type ContainerRegistry string
 
 const (
 	// AgentLatestVersion correspond to the latest stable agent release
-	AgentLatestVersion = "7.44.0"
+	AgentLatestVersion = "7.44.1"
 	// ClusterAgentLatestVersion correspond to the latest stable cluster-agent release
-	ClusterAgentLatestVersion = "7.44.0"
+	ClusterAgentLatestVersion = "7.44.1"
 
 	// GCRContainerRegistry correspond to the datadoghq GCR registry
 	GCRContainerRegistry ContainerRegistry = "gcr.io/datadoghq"
