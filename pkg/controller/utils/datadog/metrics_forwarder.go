6e1859b47727bf1e9633890f30fb043cd71d49af
Celene <celene@datadoghq.com>
2023-07-13 15:30:28 -0400

    [datadogmonitor] add support for site envvar (#838) (#840)

--- pkg/controller/utils/datadog/metrics_forwarder.go
+++ pkg/controller/utils/datadog/metrics_forwarder.go
@@ -249,7 +249,7 @@ func (mf *metricsForwarder) setupV2() error {
 	}
 
 	mf.baseURL = getbaseURLV2(dda)
-	mf.logger.Info("Got Datadog Site", "site", mf.baseURL)
+	mf.logger.Info("Got site for DatadogAgent", "site", mf.baseURL)
 
 	if dda.Spec.Global != nil && dda.Spec.Global.ClusterName != nil {
 		mf.clusterName = *dda.Spec.Global.ClusterName
