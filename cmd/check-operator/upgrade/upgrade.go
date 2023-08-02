37f8a0e69012523057cb8a6c2b42701fd831fd3c
Cedric Lamoriniere <cedric.lamoriniere@datadoghq.com>
2023-08-02 13:47:37 +0200

    fix(check-operator): do to return error if DatadogAgent not found

--- cmd/check-operator/upgrade/upgrade.go
+++ cmd/check-operator/upgrade/upgrade.go
@@ -135,8 +135,6 @@ func (o *Options) getV1Status() (common.StatusWrapper, error) {
 	err := o.Client.Get(context.TODO(), client.ObjectKey{Namespace: o.UserNamespace, Name: o.datadogAgentName}, datadogAgent)
 	if err != nil {
 		if errors.IsNotFound(err) {
-			o.printOutf("Got a not found error while getting %s/%s. Assuming this DatadogAgent CR has never been deployed in this environment", o.UserNamespace, o.datadogAgentName)
-
 			return nil, err
 		}
 
@@ -150,7 +148,6 @@ func (o *Options) getV2Status() (common.StatusWrapper, error) {
 	err := o.Client.Get(context.TODO(), client.ObjectKey{Namespace: o.UserNamespace, Name: o.datadogAgentName}, datadogAgent)
 	if err != nil {
 		if errors.IsNotFound(err) {
-			o.printOutf("Got a not found error while getting %s/%s. Assuming this DatadogAgent CR has never been deployed in this environment", o.UserNamespace, o.datadogAgentName)
 
 			return nil, err
 		}
@@ -174,6 +171,14 @@ func (o *Options) Run() error {
 			status, err = o.getV2Status()
 		} else {
 			status, err = o.getV1Status()
+		}
+		if errors.IsNotFound(err) {
+			o.printOutf("Got a not found error while getting %s/%s. Assuming this DatadogAgent CR has never been deployed in this environment", o.UserNamespace, o.datadogAgentName)
+			return true, nil
+		}
+
+		if err != nil {
+			return false, fmt.Errorf("unable to get the DatadogAgent.status, err:%w", err)
 		}
 
 		if !agentDone {
