a4bc5462f218a05cfb7b9b1b7fff725edb195194
levan-m <116471169+levan-m@users.noreply.github.com>
2023-09-11 09:57:12 -0400

    Add a startup log for V2APIEnabled flag deprecation (#915)
    * Add a startup log for V2APIEnabled flag deprecation
    
    * Update main.go
    
    Co-authored-by: Celene <celene@datadoghq.com>
    
    ---------
    
    Co-authored-by: Celene <celene@datadoghq.com>

--- main.go
+++ main.go
@@ -178,7 +178,6 @@ func main() {
 // run allow to use defer func() paradigm properly.
 // do not use `os.Exit()` in this function
 func run(opts *options) error {
-
 	// Logging setup
 	if err := customSetupLogging(*opts.logLevel, opts.logEncoder); err != nil {
 		return setupErrorf(setupLog, err, "Unable to setup the logger")
@@ -190,6 +189,12 @@ func run(opts *options) error {
 		return nil
 	}
 	version.PrintVersionLogs(setupLog)
+
+	if !opts.v2APIEnabled {
+		setupLog.Info("The `v2APIEnabled` flag is deprecated in v1.2.0+ and will be removed in a future release." +
+			"Once removed, the Datadog Operator cannot be configured to reconcile the v1alpha1 DatadogAgent CRD." +
+			"You will still be able to apply v1alpha1 with the conversion webhook enabled (`webhookEnabled`).")
+	}
 
 	if opts.profilingEnabled {
 		setupLog.Info("Starting datadog profiler")
