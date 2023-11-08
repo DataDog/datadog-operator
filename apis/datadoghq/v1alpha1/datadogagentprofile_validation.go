cf7bdf69fbe714153e1b04f78db7ea5e51d6de46
khewonc <39867936+khewonc@users.noreply.github.com>
2023-11-08 17:04:09 -0500

    Add profile validation

---
+++ apis/datadoghq/v1alpha1/datadogagentprofile_validation.go
@@ -0,0 +1,45 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-2020 Datadog, Inc.
+
+package v1alpha1
+
+import (
+	"fmt"
+)
+
+// IsValidDatadogAgentProfile is used to check if a DatadogAgentProfileSpec is valid
+func IsValidDatadogAgentProfile(spec *DatadogAgentProfileSpec) error {
+	// check that profileAffinity contains a set of requirements
+	if spec.ProfileAffinity == nil {
+		return fmt.Errorf("profileAffinity must be defined")
+	}
+	if spec.ProfileAffinity.ProfileNodeAffinity == nil {
+		return fmt.Errorf("profileNodeAffinity must be defined")
+	}
+	if len(spec.ProfileAffinity.ProfileNodeAffinity) < 1 {
+		return fmt.Errorf("profileNodeAffinity must have at least 1 requirement")
+	}
+
+	// validate config
+	if spec.Config == nil {
+		return fmt.Errorf("config must be defined")
+	}
+	if spec.Config.Override == nil {
+		return fmt.Errorf("config override must be defined")
+	}
+	if spec.Config.Override[NodeAgentComponentName] == nil {
+		return fmt.Errorf("node agent override must be defined")
+	}
+	if spec.Config.Override[NodeAgentComponentName].Containers == nil {
+		return fmt.Errorf("node agent container must be defined")
+	}
+	for name, container := range spec.Config.Override[NodeAgentComponentName].Containers {
+		if container.Resources == nil {
+			return fmt.Errorf("%s container resource must be defined", name)
+		}
+	}
+
+	return nil
+}
