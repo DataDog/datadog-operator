52350d624ea0bac39c4c60ff75e15541fbaeb914
Jennifer Chen <32009013+jennchenn@users.noreply.github.com>
2023-11-22 15:37:55 -0500

    Support `CanaryAutoPauseMaxSlowStartDuration` option (#997)
    * Add support for max slow start duration config param
    
    * Update max slow start arg name to include auto pause
    
    * fixup! Update max slow start arg name to include auto pause

--- controllers/datadogagent/component/agent/new.go
+++ controllers/datadogagent/component/agent/new.go
@@ -64,12 +64,13 @@ type ExtendedDaemonsetOptions struct {
 	MaxPodUnavailable      string
 	MaxPodSchedulerFailure string
 
-	CanaryDuration             time.Duration
-	CanaryReplicas             string
-	CanaryAutoPauseEnabled     bool
-	CanaryAutoPauseMaxRestarts int32
-	CanaryAutoFailEnabled      bool
-	CanaryAutoFailMaxRestarts  int32
+	CanaryDuration                      time.Duration
+	CanaryReplicas                      string
+	CanaryAutoPauseEnabled              bool
+	CanaryAutoPauseMaxRestarts          int32
+	CanaryAutoFailEnabled               bool
+	CanaryAutoFailMaxRestarts           int32
+	CanaryAutoPauseMaxSlowStartDuration time.Duration
 }
 
 func defaultEDSSpec(options *ExtendedDaemonsetOptions) edsv1alpha1.ExtendedDaemonSetSpec {
@@ -99,6 +100,10 @@ func defaultEDSSpec(options *ExtendedDaemonsetOptions) edsv1alpha1.ExtendedDaemo
 	spec.Strategy.Canary.AutoFail.Enabled = edsv1alpha1.NewBool(options.CanaryAutoFailEnabled)
 	if options.CanaryAutoFailMaxRestarts > 0 {
 		spec.Strategy.Canary.AutoFail.MaxRestarts = edsv1alpha1.NewInt32(options.CanaryAutoFailMaxRestarts)
+	}
+
+	if options.CanaryAutoPauseMaxSlowStartDuration != 0 {
+		spec.Strategy.Canary.AutoPause.MaxSlowStartDuration = &metav1.Duration{Duration: options.CanaryAutoPauseMaxSlowStartDuration}
 	}
 
 	spec.Strategy.Canary.AutoPause.Enabled = edsv1alpha1.NewBool(options.CanaryAutoPauseEnabled)
