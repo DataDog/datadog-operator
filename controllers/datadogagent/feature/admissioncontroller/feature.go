31d7309fe2e47ab6e2019a17e2c809251a47c8e5
levan-m <116471169+levan-m@users.noreply.github.com>
2023-12-14 13:10:19 -0500

    Update controllers/datadogagent/feature/admissioncontroller/feature.go
    Co-authored-by: Celene <celene@datadoghq.com>

--- controllers/datadogagent/feature/admissioncontroller/feature.go
+++ controllers/datadogagent/feature/admissioncontroller/feature.go
@@ -180,8 +180,8 @@ func (f *admissionControllerFeature) ManageClusterAgent(managers feature.PodTemp
 	return nil
 }
 
-// ManageMultiProcessNodeAgent allows a feature to configure the multi-process container for Node Agent's corev1.PodTemplateSpec
-// if multi-process container usage is enabled and can be used with the current feature set
+// ManageMultiProcessNodeAgent allows a feature to configure the multi-process container for the Node Agent's corev1.PodTemplateSpec
+// if multi-process container usage is enabled and can be used with the current feature set.
 // It should do nothing if the feature doesn't need to configure it.
 func (f *admissionControllerFeature) ManageMultiProcessNodeAgent(managers feature.PodTemplateManagers) error {
 	return nil
