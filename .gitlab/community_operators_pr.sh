45862e151cdc5970773819e997fdcc4d47d1e6e4
Fanny Jiang <fanny.jiang@datadoghq.com>
2024-01-08 17:28:13 -0500

    test gitlab

--- .gitlab/community_operators_pr.sh
+++ .gitlab/community_operators_pr.sh
@@ -1,68 +1,66 @@
 #!/bin/bash
 
-set +o xtrace
-
-version="$1"
+VERSION="$1" # 1st argument passed to script is the operator version
 shift
-repos=("$@") # array of repos
+REPOS=("$@") # Make an array of repos with the remaining arguments passed to script
 
-echo $version
-echo "${repos[@]}"
-
-OPERATOR_DIR="datadog-operator" # need a better name for this var
-BUNDLE_DIR="bundle"
+OPERATOR_SUBPATH="datadog-operator"
+BUNDLE_PATH="bundle"
 WORKING_DIR=$PWD
-PR_BRANCH_NAME="datadog-operator-$version"
+PR_BRANCH_NAME="datadog-operator-$VERSION"
 
 mkdir tmp
 
 clone_and_sync_fork() {
-  cd $WORKING_DIR/tmp
-  gh repo clone DataDog/$repo
-  cd ./$repo
-  gh repo sync DataDog/$repo \
-  --source $ORG/$repo \
+  cd "$WORKING_DIR"/tmp || exit
+  gh repo clone DataDog/"$repo"
+  cd ./"$repo" || exit
+  gh repo sync DataDog/"$repo" \
+  --source "$ORG"/"$repo" \
   --force
 }
 
 update_bundle() {
-  cd $WORKING_DIR/tmp/$repo
-  mkdir operators/$OPERATOR_DIR/$version
-  cp -R $CI_PROJECT_DIR/$BUNDLE_PATH/* operators/$OPERATOR_DIR/$version
+  cd "$WORKING_DIR"/tmp/"$repo" || exit
+  mkdir operators/$OPERATOR_SUBPATH/"$VERSION"
+  cp -R "$CI_PROJECT_DIR"/$BUNDLE_PATH/* operators/$OPERATOR_SUBPATH/"$VERSION"
 }
 
-#create_pr() {
-#  message="operator datadog-operator ($version)"
-#  body="operator datadog-operator ($version)"
-#  git checkout -b $PR_BRANCH_NAME
-#  git add -A
-#  git commit -s -m "$message"
-#  git push -f --set-upstream origin $PR_BRANCH_NAME
-#  gh pr create --title "$message" \
-#               --body "$body" \
-#               --repo DataDog/$repo \
-#               --base main \
-#               --draft
-#}
+create_pr() {
+  message="operator datadog-operator ($VERSION)"
+  body="operator datadog-operator ($VERSION)"
+  git checkout -b "$PR_BRANCH_NAME"
+  git add -A
+  git commit -s -m "$message"
+  git push -f --set-upstream origin "$PR_BRANCH_NAME"
+  gh pr create --title "$message" \
+               --body "$body" \
+               --repo DataDog/"$repo" \
+               --base main \
+               --draft
+}
 
 
-for repo in "${repos[@]}"
+for repo in "${REPOS[@]}"
 do
+  # set up env vars for each repo
   case "$repo" in
     community-operators | community-operators-prod)
-      ORG="k8s-operatorhub"
-      OPERATOR_DIR="datadog-operator"
+      OPERATOR_SUBPATH="datadog-operator"
       BUNDLE_PATH="bundle"
       ;;&
     community-operators-prod | redhat-marketplace-operators | certified-operators)
       ORG="redhat-openshift-ecosystem"
       ;;&
+    community-operators)
+      ORG="k8s-operatorhub"
+      ;;
     redhat-marketplace-operators)
-      OPERATOR_DIR="datadog-operator-certified-rhmp"
+      OPERATOR_SUBPATH="datadog-operator-certified-rhmp"
       BUNDLE_PATH="bundle-redhat-mp"
       ;;
     certified-operators)
-      OPERATOR_DIR="datadog-operator-certified"
+      OPERATOR_SUBPATH="datadog-operator-certified"
       BUNDLE_PATH="bundle-redhat"
       ;;
     *)
@@ -71,8 +69,9 @@ do
 
   clone_and_sync_fork
   update_bundle
-#  create_pr
+  create_pr
 
 done
 
-rm -rf $WORKING_DIR/tmp
+# clean up /tmp
+rm -rf "$WORKING_DIR"/tmp
