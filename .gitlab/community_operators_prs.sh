6c71017e659bc32175a6eb759100c8be343b56b5
Fanny Jiang <fanny.jiang@datadoghq.com>
2024-01-09 13:55:18 -0500

    fix typo, add comments, cleanup

--- .gitlab/community_operators_prs.sh
+++ .gitlab/community_operators_prs.sh
@@ -5,37 +5,45 @@ shift
 REPOS=("$@") # Make an array of repos with the remaining arguments passed to script
 
 OPERATOR_SUBPATH="datadog-operator"
-BUNDLE_PATH="bundle"
+BUNDLE_NAME="bundle"
 WORKING_DIR=$PWD
 PR_BRANCH_NAME="datadog-operator-$VERSION"
 
 mkdir tmp
 
 clone_and_sync_fork() {
+  echo "Cloning fork DataDog/$repo."
   cd "$WORKING_DIR"/tmp || exit
   gh repo clone DataDog/"$repo"
   cd ./"$repo" || exit
+
+  # sync forked repo on a new branch because of repo restrictions against merging upstream to default "main" branch
+  echo "Syncing fork DataDog/$repo with upstream $ORG/$repo."
+  git checkout -b "$PR_BRANCH_NAME"
+  git push -f --set-upstream origin $PR_BRANCH_NAME
   gh repo sync DataDog/"$repo" \
+  --branch "$PR_BRANCH_NAME" \
   --source "$ORG"/"$repo" \
   --force
 }
 
 update_bundle() {
-  cd "$WORKING_DIR"/tmp/"$repo" || exit
-  mkdir operators/$OPERATOR_SUBPATH/"$VERSION"
-  cp -R "$CI_PROJECT_DIR"/$BUNDLE_PATH/* operators/$OPERATOR_SUBPATH/"$VERSION"
+  dest_path=operators/$OPERATOR_SUBPATH/"$VERSION"
+  echo "Updating bundle at \`$dest_path\` with source: \`$BUNDLE_NAME\`"
+  mkdir "$dest_path"
+  cp -R "$CI_PROJECT_DIR"/$BUNDLE_NAME/* "$dest_path"
 }
 
 create_pr() {
-  message="operator datadog-operator ($VERSION)"
-  body="Update operator datadog-operator ($VERSION).<br><br>Pull request triggered by $GITLAB_USER_EMAIL."
-  git checkout -b "$PR_BRANCH_NAME"
+  echo "Creating pull request for repo: $ORG/$repo"
+  message="operator $OPERATOR_SUBPATH ($VERSION)"
+  body="Update operator $OPERATOR_SUBPATH ($VERSION).<br><br>Pull request triggered by $GITLAB_USER_EMAIL."
   git add -A
   git commit -s -m "$message"
   git push -f --set-upstream origin "$PR_BRANCH_NAME"
   gh pr create --title "$message" \
                --body "$body" \
-               --repo $ORG/"$repo" \
+               --repo "$ORG"/"$repo" \
                --base main
 }
 
@@ -46,7 +54,7 @@ do
   case "$repo" in
     community-operators | community-operators-prod)
       OPERATOR_SUBPATH="datadog-operator"
-      BUNDLE_PATH="bundle"
+      BUNDLE_NAME="bundle"
       ;;&
     community-operators-prod | redhat-marketplace-operators | certified-operators)
       ORG="redhat-openshift-ecosystem"
@@ -56,11 +64,11 @@ do
       ;;
     redhat-marketplace-operators)
       OPERATOR_SUBPATH="datadog-operator-certified-rhmp"
-      BUNDLE_PATH="bundle-redhat-mp"
+      BUNDLE_NAME="bundle-redhat-mp"
       ;;
     certified-operators)
       OPERATOR_SUBPATH="datadog-operator-certified"
-      BUNDLE_PATH="bundle-redhat"
+      BUNDLE_NAME="bundle-redhat"
       ;;
     *)
       ;;
