3256ff335a4eb654d8b238d86b31da6ff301990e
Fanny Jiang <fanny.jiang@datadoghq.com>
2024-03-20 16:55:54 -0400

    fix imgpullsecret syntax

--- test/e2e/const.go
+++ test/e2e/const.go
@@ -66,7 +66,8 @@ func operatorTransformationFunc() func(state map[string]interface{}, opts ...pul
 		if imageTag != "" && state["kind"] == "Deployment" && name == "datadog-operator-manager" {
 			template := state["spec"].(map[string]interface{})["template"]
 			templateSpec := template.(map[string]interface{})["spec"]
-			templateSpec.(map[string]interface{})["imagePullSecrets"] = imagePullSecretName
+			specImgPullSecrets := templateSpec.(map[string]interface{})["imagePullSecrets"].([]map[string]interface{})
+			specImgPullSecrets[0] = map[string]interface{}{"name": imagePullSecretName}
 			containers := templateSpec.(map[string]interface{})["containers"]
 			container := containers.([]interface{})[0]
 			container.(map[string]interface{})["image"] = imageTag
