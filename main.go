196a769bfa478a753829fa797f15e3a50f851198
Levan Machablishvili <levan.machablishvili@datadoghq.com>
2024-05-24 15:03:43 -0400

    Inlucde milliseconds in log timestamps

--- main.go
+++ main.go
@@ -364,7 +364,7 @@ func cacheOptions() cache.Options {
 func customSetupLogging(logLevel zapcore.Level, logEncoder string) error {
 	encoderConfig := zap.NewProductionEncoderConfig()
 	encoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
-	encoderConfig.EncodeTime = zapcore.RFC3339TimeEncoder
+	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
 
 	var encoder zapcore.Encoder
 	switch logEncoder {
