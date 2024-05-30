977490581bab031eeeff9e42023eed9c6212b096
kisungyi92 <101687848+kisungyi92@users.noreply.github.com>
2024-05-30 15:04:36 -0400

    [CECO-1067] DDA config for Admission Controller sidecar injection using selector and profile (#1209)
    * Adding selectors and profiles
    
    * changing some terms and Editing selector profile logics in AC feature
    
    * Editing some comments on types, chaning profile logic in feature
    
    * Repositioning AgentSidecarInjectionConfig in type
    
    * Editing some terms in selector and profile type
    
    * Adding generate code
    
    /merge

--- controllers/datadogagent/feature/admissioncontroller/feature.go
+++ controllers/datadogagent/feature/admissioncontroller/feature.go
@@ -6,6 +6,8 @@
 package admissioncontroller
 
 import (
+	"encoding/json"
+
 	apicommon "github.com/DataDog/datadog-operator/apis/datadoghq/common"
 	"github.com/DataDog/datadog-operator/apis/datadoghq/common/v1"
 	"github.com/DataDog/datadog-operator/apis/datadoghq/v1alpha1"
@@ -50,6 +52,8 @@ type AgentSidecarInjectionConfig struct {
 	registry                         string
 	imageName                        string
 	imageTag                         string
+	selectors                        []*v2alpha1.Selector
+	profiles                         []*v2alpha1.Profile
 }
 
 func buildAdmissionControllerFeature(options *feature.Options) feature.Feature {
@@ -154,6 +158,42 @@ func (f *admissionControllerFeature) Configure(dda *v2alpha1.DatadogAgent) (reqC
 				f.agentSidecarConfig.imageTag = sidecarConfig.Image.Tag
 			} else if ok && componentOverride.Image != nil {
 				f.agentSidecarConfig.imageTag = componentOverride.Image.Tag
+			}
+
+			// Assemble agent sidecar selectors.
+			for _, selector := range sidecarConfig.Selectors {
+				newSelector := &v2alpha1.Selector{}
+
+				if selector.NamespaceSelector != nil {
+					newSelector.NamespaceSelector = &metav1.LabelSelector{
+						MatchLabels:      selector.NamespaceSelector.MatchLabels,
+						MatchExpressions: selector.NamespaceSelector.MatchExpressions,
+					}
+				}
+
+				if selector.ObjectSelector != nil {
+					newSelector.ObjectSelector = &metav1.LabelSelector{
+						MatchLabels:      selector.ObjectSelector.MatchLabels,
+						MatchExpressions: selector.ObjectSelector.MatchExpressions,
+					}
+				}
+
+				if newSelector.NamespaceSelector != nil || newSelector.ObjectSelector != nil {
+					f.agentSidecarConfig.selectors = append(f.agentSidecarConfig.selectors, newSelector)
+				}
+			}
+
+			// Assemble agent sidecar profiles.
+			for _, profile := range sidecarConfig.Profiles {
+				if len(profile.EnvVars) > 0 || profile.ResourceRequirements != nil {
+					newProfile := &v2alpha1.Profile{
+						EnvVars: profile.EnvVars,
+					}
+					if profile.ResourceRequirements != nil {
+						newProfile.ResourceRequirements = profile.ResourceRequirements
+					}
+					f.agentSidecarConfig.profiles = append(f.agentSidecarConfig.profiles, newProfile)
+				}
 			}
 		}
 
@@ -305,6 +345,28 @@ func (f *admissionControllerFeature) ManageClusterAgent(managers feature.PodTemp
 			managers.EnvVar().AddEnvVarToContainer(common.ClusterAgentContainerName, &corev1.EnvVar{
 				Name:  apicommon.DDAdmissionControllerAgentSidecarImageTag,
 				Value: f.agentSidecarConfig.imageTag,
+			})
+		}
+
+		if len(f.agentSidecarConfig.selectors) > 0 {
+			selectorsJSON, err := json.Marshal(f.agentSidecarConfig.selectors)
+			if err != nil {
+				return err
+			}
+			managers.EnvVar().AddEnvVarToContainer(common.ClusterAgentContainerName, &corev1.EnvVar{
+				Name:  apicommon.DDAdmissionControllerAgentSidecarSelectors,
+				Value: string(selectorsJSON),
+			})
+		}
+
+		if len(f.agentSidecarConfig.profiles) > 0 {
+			profilesJSON, err := json.Marshal(f.agentSidecarConfig.profiles)
+			if err != nil {
+				return err
+			}
+			managers.EnvVar().AddEnvVarToContainer(common.ClusterAgentContainerName, &corev1.EnvVar{
+				Name:  apicommon.DDAdmissionControllerAgentSidecarProfiles,
+				Value: string(profilesJSON),
 			})
 		}
 
