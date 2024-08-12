80d38ab35c5d1b6cac5be6dcc4a228840c306be2
Eman <eman.okyere@datadoghq.com>
2024-08-12 13:04:35 -0400

    removed legacy support for queryValue

--- apis/datadoghq/v1alpha1/datadogdashboard_types.go
+++ apis/datadoghq/v1alpha1/datadogdashboard_types.go
@@ -686,34 +686,14 @@ type TimeseriesBackground struct {
 type QueryValueWidgetRequest struct {
 	// Aggregator used for the request.
 	Aggregator *datadogV1.WidgetAggregator `json:"aggregator,omitempty"`
-	// The log query.
-	ApmQuery *LogQueryDefinition `json:"apmQuery,omitempty"`
-	// The log query.
-	AuditQuery *LogQueryDefinition `json:"auditQuery,omitempty"`
 	// List of conditional formats.
 	ConditionalFormats []WidgetConditionalFormat `json:"conditionalFormats,omitempty"`
-	// The log query.
-	EventQuery *LogQueryDefinition `json:"eventQuery,omitempty"`
 	// List of formulas that operate on queries.
 	Formulas []WidgetFormula `json:"formulas,omitempty"`
-	// The log query.
-	LogQuery *LogQueryDefinition `json:"logQuery,omitempty"`
-	// The log query.
-	NetworkQuery *LogQueryDefinition `json:"networkQuery,omitempty"`
-	// The process query to use in the widget.
-	ProcessQuery *ProcessQueryDefinition `json:"processQuery,omitempty"`
-	// The log query.
-	ProfileMetricsQuery *LogQueryDefinition `json:"profileMetricsQuery,omitempty"`
-	// TODO.
-	Q *string `json:"q,omitempty"`
 	// List of queries that can be returned directly or used in formulas.
 	Queries []FormulaAndFunctionQueryDefinition `json:"queries,omitempty"`
 	// Timeseries, scalar, or event list response. Event list response formats are supported by Geomap widgets.
 	ResponseFormat *datadogV1.FormulaAndFunctionResponseFormat `json:"responseFormat,omitempty"`
-	// The log query.
-	RumQuery *LogQueryDefinition `json:"rumQuery,omitempty"`
-	// The log query.
-	SecurityQuery *LogQueryDefinition `json:"securityQuery,omitempty"`
 }
 
 // DatadogDashboardList contains a list of DatadogDashboard
