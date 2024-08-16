8bbf45607f4f956e3c8a0e2a4499ec37cd21e28a
Celene <celene@datadoghq.com>
2024-08-16 11:57:08 -0400

    [cleanup] rm unused apis code (#1354)

--- pkg/plugin/common/statuswrapper.go
+++ pkg/plugin/common/statuswrapper.go
@@ -7,7 +7,6 @@ package common
 
 import (
 	commonv1 "github.com/DataDog/datadog-operator/apis/datadoghq/common/v1"
-	"github.com/DataDog/datadog-operator/apis/datadoghq/v1alpha1"
 	"github.com/DataDog/datadog-operator/apis/datadoghq/v2alpha1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -18,42 +17,6 @@ type StatusWrapper interface {
 	GetClusterAgentStatus() *commonv1.DeploymentStatus
 	GetClusterChecksRunnerStatus() *commonv1.DeploymentStatus
 	GetStatusCondition() []metav1.Condition
-}
-
-func NewV1StatusWrapper(dda *v1alpha1.DatadogAgent) StatusWrapper {
-	return &v1StatusWrapper{dda}
-}
-
-type v1StatusWrapper struct {
-	dda *v1alpha1.DatadogAgent
-}
-
-func (sw v1StatusWrapper) GetObjectMeta() metav1.Object { return sw.dda.GetObjectMeta() }
-func (sw v1StatusWrapper) GetStatusCondition() []metav1.Condition {
-	var newConditionsList []metav1.Condition
-	for _, condition := range sw.dda.Status.Conditions {
-		newConditionsList = append(newConditionsList, AgentConditionToMetaV1(condition))
-	}
-	return newConditionsList
-}
-
-func (sw v1StatusWrapper) GetAgentStatus() *commonv1.DaemonSetStatus {
-	if sw.dda != nil {
-		return sw.dda.Status.Agent
-	}
-	return nil
-}
-func (sw v1StatusWrapper) GetClusterAgentStatus() *commonv1.DeploymentStatus {
-	if sw.dda != nil {
-		return sw.dda.Status.ClusterAgent
-	}
-	return nil
-}
-func (sw v1StatusWrapper) GetClusterChecksRunnerStatus() *commonv1.DeploymentStatus {
-	if sw.dda != nil {
-		return sw.dda.Status.ClusterChecksRunner
-	}
-	return nil
 }
 
 func NewV2StatusWrapper(dda *v2alpha1.DatadogAgent) StatusWrapper {
@@ -86,14 +49,4 @@ func (sw v2StatusWrapper) GetClusterChecksRunnerStatus() *commonv1.DeploymentSta
 		return sw.dda.Status.ClusterChecksRunner
 	}
 	return nil
-}
-
-func AgentConditionToMetaV1(condition v1alpha1.DatadogAgentCondition) metav1.Condition {
-	return metav1.Condition{
-		Type:               string(condition.Type),
-		Status:             metav1.ConditionStatus(condition.Status),
-		LastTransitionTime: condition.LastTransitionTime,
-		Reason:             condition.Reason,
-		Message:            condition.Message,
-	}
 }
