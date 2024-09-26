c1a2aeeb1314edd0e1209a32bb876d2b605054b8
mrmcpat <109171317+mrdoggopat@users.noreply.github.com>
2024-09-26 15:49:38 -0400

    Update internal/controller/datadogagent/feature/apm/feature.go
    Co-authored-by: khewonc <39867936+khewonc@users.noreply.github.com>

--- internal/controller/datadogagent/feature/apm/feature.go
+++ internal/controller/datadogagent/feature/apm/feature.go
@@ -176,7 +176,6 @@ func (f *apmFeature) shouldEnableLanguageDetection() bool {
 // ManageDependencies allows a feature to manage its dependencies.
 // Feature's dependencies should be added in the store.
 func (f *apmFeature) ManageDependencies(managers feature.ResourceManagers, components feature.RequiredComponents) error {
-
 	platformInfo := managers.Store().GetPlatformInfo()
 	// agent local service
 	if common.ShouldCreateAgentLocalService(platformInfo.GetVersionInfo(), f.forceEnableLocalService) {
