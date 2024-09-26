44f8ea6ad48a65a5fe05c6295e418750b7c9703e
mrmcpat <109171317+mrdoggopat@users.noreply.github.com>
2024-09-26 15:49:44 -0400

    Update internal/controller/datadogagent/feature/dogstatsd/feature.go
    Co-authored-by: khewonc <39867936+khewonc@users.noreply.github.com>

--- internal/controller/datadogagent/feature/dogstatsd/feature.go
+++ internal/controller/datadogagent/feature/dogstatsd/feature.go
@@ -101,7 +101,6 @@ func (f *dogstatsdFeature) Configure(dda *v2alpha1.DatadogAgent) (reqComp featur
 // ManageDependencies allows a feature to manage its dependencies.
 // Feature's dependencies should be added in the store.
 func (f *dogstatsdFeature) ManageDependencies(managers feature.ResourceManagers, components feature.RequiredComponents) error {
-
 	platformInfo := managers.Store().GetPlatformInfo()
 	// agent local service
 	if common.ShouldCreateAgentLocalService(platformInfo.GetVersionInfo(), f.forceEnableLocalService) {
