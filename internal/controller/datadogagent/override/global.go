cfaff9d57feebab4bd755772eb4c7e9b9c2a952a
Timothée Bavelier <97530782+tbavelier@users.noreply.github.com>
2024-10-03 11:02:25 +0200

    [CECO-743] Secret backend configuration (#1395)
    * global secret backend config
    
    * tests
    
    * fix DefaultAgentResourceSuffix that changed from apicommon to v2alpha1

--- internal/controller/datadogagent/override/global.go
+++ internal/controller/datadogagent/override/global.go
@@ -10,6 +10,8 @@ import (
 	"fmt"
 	"path/filepath"
 
+	"strconv"
+
 	apicommon "github.com/DataDog/datadog-operator/api/datadoghq/common"
 	"github.com/DataDog/datadog-operator/api/datadoghq/v2alpha1"
 	apiutils "github.com/DataDog/datadog-operator/api/utils"
@@ -17,6 +19,8 @@ import (
 	"github.com/DataDog/datadog-operator/internal/controller/datadogagent/feature"
 	"github.com/DataDog/datadog-operator/internal/controller/datadogagent/object/volume"
 	"github.com/DataDog/datadog-operator/pkg/defaulting"
+	"github.com/DataDog/datadog-operator/pkg/kubernetes/rbac"
+	rbacv1 "k8s.io/api/rbac/v1"
 
 	"github.com/go-logr/logr"
 	corev1 "k8s.io/api/core/v1"
@@ -309,6 +313,80 @@ func applyGlobalSettings(logger logr.Logger, manager feature.PodTemplateManagers
 					},
 				)
 				manager.Volume().AddVolume(&runtimeVol)
+			}
+		}
+	}
+
+	// Apply SecretBackend config
+	if config.SecretBackend != nil {
+		// Set secret backend command
+		manager.EnvVar().AddEnvVar(&corev1.EnvVar{
+			Name:  apicommon.DDSecretBackendCommand,
+			Value: apiutils.StringValue(config.SecretBackend.Command),
+		})
+
+		// Set secret backend arguments
+		manager.EnvVar().AddEnvVar(&corev1.EnvVar{
+			Name:  apicommon.DDSecretBackendArguments,
+			Value: apiutils.StringValue(config.SecretBackend.Args),
+		})
+
+		// Set secret backend timeout
+		if config.SecretBackend.Timeout != nil {
+			manager.EnvVar().AddEnvVar(&corev1.EnvVar{
+				Name:  apicommon.DDSecretBackendTimeout,
+				Value: strconv.FormatInt(int64(*config.SecretBackend.Timeout), 10),
+			})
+		}
+
+		var componentSaName string
+		switch componentName {
+		case v2alpha1.ClusterAgentComponentName:
+			componentSaName = v2alpha1.GetClusterAgentServiceAccount(dda)
+		case v2alpha1.NodeAgentComponentName:
+			componentSaName = v2alpha1.GetAgentServiceAccount(dda)
+		case v2alpha1.ClusterChecksRunnerComponentName:
+			componentSaName = v2alpha1.GetClusterChecksRunnerServiceAccount(dda)
+		}
+
+		agentName := dda.GetName()
+		agentNs := dda.GetNamespace()
+		rbacSuffix := "secret-backend"
+
+		// Set global RBAC config (only if specific roles are not defined)
+		if apiutils.BoolValue(config.SecretBackend.EnableGlobalPermissions) && config.SecretBackend.Roles == nil {
+
+			var secretBackendGlobalRBACPolicyRules = []rbacv1.PolicyRule{
+				{
+					APIGroups: []string{rbac.CoreAPIGroup},
+					Resources: []string{rbac.SecretsResource},
+					Verbs:     []string{rbac.GetVerb},
+				},
+			}
+
+			roleName := fmt.Sprintf("%s-%s-%s", agentNs, agentName, rbacSuffix)
+
+			if err := resourcesManager.RBACManager().AddClusterPolicyRules(agentNs, roleName, componentSaName, secretBackendGlobalRBACPolicyRules); err != nil {
+				logger.Error(err, "Error adding cluster-wide secrets RBAC policy")
+			}
+		}
+
+		// Set specific roles for the secret backend
+		if config.SecretBackend.Roles != nil {
+			for _, role := range config.SecretBackend.Roles {
+				secretNs := apiutils.StringValue(role.Namespace)
+				roleName := fmt.Sprintf("%s-%s-%s", secretNs, agentName, rbacSuffix)
+				policyRule := []rbacv1.PolicyRule{
+					{
+						APIGroups:     []string{rbac.CoreAPIGroup},
+						Resources:     []string{rbac.SecretsResource},
+						ResourceNames: role.Secrets,
+						Verbs:         []string{rbac.GetVerb},
+					},
+				}
+				if err := resourcesManager.RBACManager().AddPolicyRules(secretNs, roleName, componentSaName, policyRule, agentNs); err != nil {
+					logger.Error(err, "Error adding secrets RBAC policy")
+				}
 			}
 		}
 	}
