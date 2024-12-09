675d628496ec26947ac9328d3639796ee7889c56
Sarah Wang <sarah.wang2009@gmail.com>
2024-12-09 11:39:40 -0500

    add parameter for testing go module

--- api/datadoghq/v2alpha1/datadogagent_types.go
+++ api/datadoghq/v2alpha1/datadogagent_types.go
@@ -1163,6 +1163,8 @@ type DeploymentStatus struct {
 
 // GlobalConfig is a set of parameters that are used to configure all the components of the Datadog Operator.
 type GlobalConfig struct {
+	TestString *string `json:"testString,omitempty"`
+
 	// Credentials defines the Datadog credentials used to submit data to/query data from Datadog.
 	Credentials *DatadogCredentials `json:"credentials,omitempty"`
 
