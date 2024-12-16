d353bf6d493b033c4ea5e6cc4c336aef8513d192
Timothée Bavelier <timothee.bavelier@datadoghq.com>
2024-12-16 13:07:31 +0100

    update agent to 7.60.0

--- pkg/defaulting/images.go
+++ pkg/defaulting/images.go
@@ -16,9 +16,9 @@ type ContainerRegistry string
 
 const (
 	// AgentLatestVersion corresponds to the latest stable agent release
-	AgentLatestVersion = "7.59.0"
+	AgentLatestVersion = "7.60.0"
 	// ClusterAgentLatestVersion corresponds to the latest stable cluster-agent release
-	ClusterAgentLatestVersion = "7.59.0"
+	ClusterAgentLatestVersion = "7.60.0"
 	// FIPSProxyLatestVersion corresponds to the latest stable fips-proxy release
 	FIPSProxyLatestVersion = "1.1.6"
 	// GCRContainerRegistry corresponds to the datadoghq GCR registry
