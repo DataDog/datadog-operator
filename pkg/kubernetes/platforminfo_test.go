c17ec1de2615638a9d7c3d15b31fdfd1d70d35d2
Sarah Wang <sarah.wang@datadoghq.com>
2024-12-19 10:32:17 -0500

    Revert "[cleanup] remove unused v1beta1 version of PDB (#1468)" (#1580)
    * Revert "[cleanup] remove unused v1beta1 version of PDB (#1468)"
    
    This reverts commit 614b10f7c754cd50d1d95f4a0e9e18b975204a9c.
    
    * use go convention
    
    * support v1beta1 for pdbs
    
    * go convetion
    
    * Revert "support v1beta1 for pdbs"
    
    This reverts commit 13b2cab2b2596c6f690d468f0803af29f48f76c3.
    
    * Reapply "support v1beta1 for pdbs"
    
    This reverts commit 59715f165a97a2e45c10d3ccf5ea53f1848db4c5.
    
    ---------
    
    Co-authored-by: Timothée Bavelier <timothee.bavelier@datadoghq.com>
    Co-authored-by: Timothée Bavelier <97530782+tbavelier@users.noreply.github.com>

--- pkg/kubernetes/platforminfo_test.go
+++ pkg/kubernetes/platforminfo_test.go
@@ -7,6 +7,124 @@ import (
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+func Test_createPlatformInfoFromAPIObjects(t *testing.T) {
+	tests := []struct {
+		name                string
+		tag                 string
+		apiGroups           []*v1.APIGroup
+		apiResourceList     []*v1.APIResourceList
+		useV1Beta1PDB       bool
+		pdbPreferredVersion string
+		pdbOtherVersion     string
+	}{
+		{
+			name: "v1 preferred, PDB v1 prferred, PDB v1beta1 not proferred",
+			apiGroups: []*v1.APIGroup{
+				newApiGroupPointer(
+					v1.APIGroup{
+						Name: "policy",
+						Versions: []v1.GroupVersionForDiscovery{
+							{
+								GroupVersion: "policy/v1",
+							},
+							{
+								GroupVersion: "policy/v1beta1",
+							},
+						},
+						PreferredVersion: v1.GroupVersionForDiscovery{
+							GroupVersion: "policy/v1",
+						},
+					},
+				),
+			},
+			apiResourceList:     createDefaultApiResourceList(),
+			useV1Beta1PDB:       false,
+			pdbPreferredVersion: "policy/v1",
+			pdbOtherVersion:     "policy/v1beta1",
+		},
+		{
+			name: "v1beta1 preferred, PDB PDB v1 not proferred",
+			tag:  "tag 1",
+			apiGroups: []*v1.APIGroup{
+				newApiGroupPointer(
+					v1.APIGroup{
+						Name: "policy",
+						Versions: []v1.GroupVersionForDiscovery{
+							{
+								GroupVersion: "policy/v1",
+							},
+							{
+								GroupVersion: "policy/v1beta1",
+							},
+						},
+						PreferredVersion: v1.GroupVersionForDiscovery{
+							GroupVersion: "policy/v1beta1",
+						},
+					},
+				),
+			},
+			apiResourceList:     createDefaultApiResourceList(),
+			useV1Beta1PDB:       true,
+			pdbPreferredVersion: "policy/v1beta1",
+			pdbOtherVersion:     "policy/v1",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			platformInfo := NewPlatformInfo(nil, tt.apiGroups, tt.apiResourceList)
+			assert.Equal(t, tt.useV1Beta1PDB, platformInfo.UseV1Beta1PDB())
+			assert.Equal(t, tt.pdbPreferredVersion, platformInfo.apiPreferredVersions["PodDisruptionBudget"])
+			assert.Equal(t, tt.pdbOtherVersion, platformInfo.apiOtherVersions["PodDisruptionBudget"])
+		})
+	}
+}
+
+func Test_getPDBFlag(t *testing.T) {
+	tests := []struct {
+		name          string
+		preferred     map[string]string
+		other         map[string]string
+		useV1Beta1PDB bool
+	}{
+		{
+			name: "Chooses preferred version of PodDisruptionBudget",
+			preferred: map[string]string{
+				"PodDisruptionBudget": "policy/v1",
+			},
+			other: map[string]string{
+				"PodDisruptionBudget": "policy/v1beta1",
+			},
+			useV1Beta1PDB: false,
+		},
+		{
+			name: "Chooses preferred version of PodDisruptionBudget",
+			preferred: map[string]string{
+				"PodDisruptionBudget": "policy/v1beta1",
+			},
+			other: map[string]string{
+				"PodDisruptionBudget": "policy/v1",
+			},
+			useV1Beta1PDB: true,
+		},
+		{
+			name: "Unrecognized preferred version, defaults to v1",
+			preferred: map[string]string{
+				"PodDisruptionBudget": "xyz",
+			},
+			other:         map[string]string{},
+			useV1Beta1PDB: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			platformInfo := NewPlatformInfoFromVersionMaps(nil, tt.preferred, tt.other)
+			assert.Equal(t, tt.useV1Beta1PDB, platformInfo.UseV1Beta1PDB())
+		})
+	}
+}
+
 func Test_getDatadogAgentVersions(t *testing.T) {
 	tests := []struct {
 		name            string
@@ -92,6 +210,41 @@ func Test_getDatadogAgentVersions(t *testing.T) {
 	}
 }
 
+func createDefaultApiResourceList() []*v1.APIResourceList {
+	return []*v1.APIResourceList{
+		newApiResourceListPointer(
+			v1.APIResourceList{
+				GroupVersion: "policy/v1",
+				APIResources: []v1.APIResource{
+					{
+						Kind: "PodDisruptionBudget",
+					},
+				},
+			},
+		),
+		newApiResourceListPointer(
+			v1.APIResourceList{
+				GroupVersion: "policy/v1beta1",
+				APIResources: []v1.APIResource{
+					{
+						Kind: "PodDisruptionBudget",
+					},
+				},
+			},
+		),
+		newApiResourceListPointer(
+			v1.APIResourceList{
+				GroupVersion: "datadoghq/v1alpha1",
+				APIResources: []v1.APIResource{
+					{
+						Kind: "DatadogAgent",
+					},
+				},
+			},
+		),
+	}
+}
+
 func createDatadogAgentResourceList() []*v1.APIResourceList {
 	return []*v1.APIResourceList{
 		newApiResourceListPointer(
@@ -123,4 +276,13 @@ func newApiGroupPointer(apiGroup v1.APIGroup) *v1.APIGroup {
 
 func newApiResourceListPointer(apiResourceList v1.APIResourceList) *v1.APIResourceList {
 	return &apiResourceList
+}
+
+func containsObjectKind(list []ObjectKind, s ObjectKind) bool {
+	for _, v := range list {
+		if v == s {
+			return true
+		}
+	}
+	return false
 }
