ba813ff7d44ebc15f9d9d6ce830dec098f0b536d
Timothée Bavelier <timothee.bavelier@datadoghq.com>
2025-01-08 15:04:24 +0100

    apm e2e test refactor

--- test/e2e/tests/k8s_suite/k8s_suite_test.go
+++ test/e2e/tests/k8s_suite/k8s_suite_test.go
@@ -201,8 +201,7 @@ func (s *k8sSuite) TestGenericK8s() {
 			utils.VerifyAgentPods(s.T(), c, common.NamespaceName, s.Env().KubernetesCluster.Client(), common.NodeAgentSelector+",agent.datadoghq.com/name=dda-autodiscovery")
 
 			// check agent pods for http check
-			agentPods, err := s.Env().KubernetesCluster.Client().CoreV1().Pods(common.NamespaceName).List(context.TODO(), metav1.ListOptions{LabelSelector: common.NodeAgentSelector + ",agent.datadoghq.com/name=dda-autodiscovery",
-				FieldSelector: "status.phase=Running"})
+			agentPods, err := s.Env().KubernetesCluster.Client().CoreV1().Pods(common.NamespaceName).List(context.TODO(), metav1.ListOptions{LabelSelector: common.NodeAgentSelector + ",agent.datadoghq.com/name=dda-autodiscovery"})
 			assert.NoError(c, err)
 
 			for _, pod := range agentPods.Items {
@@ -254,12 +253,76 @@ func (s *k8sSuite) TestGenericK8s() {
 			s.verifyAPILogs()
 		}, 900*time.Second, 15*time.Second, "could not valid logs collection in time")
 	})
+
+	s.T().Run("APM hostPort k8s service UDP works", func(t *testing.T) {
+		var apmAgentSelector = ",agent.datadoghq.com/name=datadog-agent-apm"
+
+		ddaConfigPath, err := common.GetAbsPath(filepath.Join(common.ManifestsPath, "apm", "datadog-agent-apm.yaml"))
+		assert.NoError(s.T(), err)
+
+		ddaOpts := []agentwithoperatorparams.Option{
+			agentwithoperatorparams.WithDDAConfig(agentwithoperatorparams.DDAConfig{
+				Name:         "datadog-agent-apm",
+				YamlFilePath: ddaConfigPath,
+			}),
+		}
+		ddaOpts = append(ddaOpts, defaultDDAOpts...)
+
+		provisionerOptions := []provisioners.KubernetesProvisionerOption{
+			provisioners.WithTestName("e2e-operator-apm"),
+			provisioners.WithDDAOptions(ddaOpts...),
+			provisioners.WithYAMLWorkload(provisioners.YAMLWorkload{
+				Name: "tracegen-deploy",
+				Path: strings.Join([]string{common.ManifestsPath, "apm", "tracegen-deploy.yaml"}, "/"),
+			}),
+			provisioners.WithLocal(s.local),
+		}
+		provisionerOptions = append(provisionerOptions, defaultProvisionerOpts...)
+
+		// Deploy trace generator
+		s.UpdateEnv(provisioners.KubernetesProvisioner(provisionerOptions...))
+
+		// Verify traces collection on agent pod
+		s.EventuallyWithTf(func(c *assert.CollectT) {
+			// Verify tracegen deployment is running
+			utils.VerifyNumPodsForSelector(s.T(), c, common.NamespaceName, s.Env().KubernetesCluster.Client(), 1, "app=tracegen-tribrid")
+
+			// Verify agent pods are running
+			utils.VerifyAgentPods(s.T(), c, common.NamespaceName, s.Env().KubernetesCluster.Client(), common.NodeAgentSelector+apmAgentSelector)
+			agentPods, err := s.Env().KubernetesCluster.Client().CoreV1().Pods(common.NamespaceName).List(context.TODO(), metav1.ListOptions{LabelSelector: common.NodeAgentSelector + apmAgentSelector})
+			assert.NoError(c, err)
+
+			// This works because we have a single Agent pod (so located on same node as tracegen)
+			// Otherwise, we would need to deploy tracegen on the same node as the Agent pod / as a DaemonSet
+			for _, pod := range agentPods.Items {
+
+				output, _, err := s.Env().KubernetesCluster.KubernetesClient.PodExec(common.NamespaceName, pod.Name, "agent", []string{"agent", "status", "apm agent", "-j"})
+				assert.NoError(c, err)
+
+				utils.VerifyAgentTraces(c, output)
+			}
+
+			// Verify traces collection with API client
+			s.verifyAPITraces(c)
+		}, 600*time.Second, 15*time.Second, "could not validate traces on agent pod") // TODO: check duration
+	})
 }
 
 func (s *k8sSuite) verifyAPILogs() {
 	logs, err := s.Env().FakeIntake.Client().FilterLogs("agent")
 	s.Assert().NoError(err)
 	s.Assert().NotEmptyf(logs, fmt.Sprintf("Expected fake intake-ingested logs to not be empty: %s", err))
+}
+
+func (s *k8sSuite) verifyAPITraces(c *assert.CollectT) {
+	_, err := s.Env().FakeIntake.Client().GetTraces()
+	assert.NoError(c, err)
+
+	// for _, trace := range traces {
+	// 	tags := lo.MapToSlice(trace.Tags, func(k string, v string) string {
+	// 		return k + ":" + v
+	// 	})
+
 }
 
 func (s *k8sSuite) verifyKSMCheck(c *assert.CollectT) {
