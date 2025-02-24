7eb49dcbf9883fdf90c057b53818f071709c7d90
Celene <celene@datadoghq.com>
2025-02-24 10:29:25 -0500

    bump default agent images to 7.63.0 (#1722)

--- pkg/defaulting/images.go
+++ pkg/defaulting/images.go
@@ -16,9 +16,9 @@ type ContainerRegistry string
 
 const (
 	// AgentLatestVersion corresponds to the latest stable agent release
-	AgentLatestVersion = "7.62.2"
+	AgentLatestVersion = "7.63.0"
 	// ClusterAgentLatestVersion corresponds to the latest stable cluster-agent release
-	ClusterAgentLatestVersion = "7.62.2"
+	ClusterAgentLatestVersion = "7.63.0"
 	// FIPSProxyLatestVersion corresponds to the latest stable fips-proxy release
 	FIPSProxyLatestVersion = "1.1.7"
 	// GCRContainerRegistry corresponds to the datadoghq GCR registry
