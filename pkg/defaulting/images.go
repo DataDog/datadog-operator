0161ab797eea40131618cec05090159ff7190b9b
Mackenzie <63265430+mackjmr@users.noreply.github.com>
2025-02-25 14:00:13 +0100

    Update pkg/defaulting/images.go
    Co-authored-by: Celene <celene@datadoghq.com>

--- pkg/defaulting/images.go
+++ pkg/defaulting/images.go
@@ -39,8 +39,6 @@ const (
 	// Default Image names
 	DefaultAgentImageName        string = "agent"
 	DefaultClusterAgentImageName string = "cluster-agent"
-	AgentDevImageName                   = "datadog/agent-dev"
-	// Nightly dev image tag for otel agent
 	OTelAgentBetaTag = "7.63.0-ot-beta-jmx"
 )
 
