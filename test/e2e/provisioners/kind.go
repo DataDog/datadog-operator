87f5aad3ced32653666587f6fed466291558895a
Kevin Fairise <kevin.fairise@datadoghq.com>
2025-03-25 13:47:16 +0100

    Use ubuntu2004

--- test/e2e/provisioners/kind.go
+++ test/e2e/provisioners/kind.go
@@ -7,13 +7,17 @@ package provisioners
 
 import (
 	"fmt"
-	"github.com/DataDog/test-infra-definitions/scenarios/aws/ec2"
 	"os"
 	"strings"
+
+	tifos "github.com/DataDog/test-infra-definitions/components/os"
+	"github.com/DataDog/test-infra-definitions/scenarios/aws/ec2"
 
 	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/environments"
 	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/provisioners"
 	awskubernetes "github.com/DataDog/datadog-agent/test/new-e2e/pkg/provisioners/aws/kubernetes"
+
+	"path/filepath"
 
 	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/runner"
 	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/utils/optional"
@@ -34,7 +38,6 @@ import (
 	"github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/yaml"
 	"github.com/pulumi/pulumi/sdk/v3/go/auto"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
-	"path/filepath"
 )
 
 const (
@@ -94,7 +97,7 @@ func newAWSK8sProvisionerOpts(params *KubernetesProvisionerParams) []awskubernet
 		awskubernetes.WithExtraConfigParams(extraConfig),
 		awskubernetes.WithWorkloadApp(KustomizeWorkloadAppFunc(params.testName, params.kustomizeResources)),
 		awskubernetes.WithFakeIntakeOptions(params.fakeintakeOptions...),
-		awskubernetes.WithEC2VMOptions([]ec2.VMOption{ec2.WithUserData(UserData)}...),
+		awskubernetes.WithEC2VMOptions([]ec2.VMOption{ec2.WithOS(tifos.Ubuntu2004), ec2.WithUserData(UserData)}...),
 	}
 
 	for _, yamlWorkload := range params.yamlWorkloads {
