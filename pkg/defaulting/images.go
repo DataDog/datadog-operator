3d1fc6cce69e8fff547ea0c62df366b2f7475d3d
Levan Machablishvili <levan.machablishvili@datadoghq.com>
2025-04-09 09:44:37 -0400

    Bump Agent to 7.64.2

--- pkg/defaulting/images.go
+++ pkg/defaulting/images.go
@@ -16,9 +16,9 @@ type ContainerRegistry string
 
 const (
 	// AgentLatestVersion corresponds to the latest stable agent release
-	AgentLatestVersion = "7.64.1"
+	AgentLatestVersion = "7.64.2"
 	// ClusterAgentLatestVersion corresponds to the latest stable cluster-agent release
-	ClusterAgentLatestVersion = "7.64.1"
+	ClusterAgentLatestVersion = "7.64.2"
 	// FIPSProxyLatestVersion corresponds to the latest stable fips-proxy release
 	FIPSProxyLatestVersion = "1.1.9"
 	// GCRContainerRegistry corresponds to the datadoghq GCR registry
