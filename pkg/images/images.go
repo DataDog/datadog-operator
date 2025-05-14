c2722ccdb15b2a59990402968cc5d7f5139f85a8
khewonc <39867936+khewonc@users.noreply.github.com>
2025-05-14 11:33:15 -0400

    Update agent images to 7.65.2

--- pkg/images/images.go
+++ pkg/images/images.go
@@ -15,9 +15,9 @@ import (
 
 const (
 	// AgentLatestVersion corresponds to the latest stable agent release
-	AgentLatestVersion = "7.64.3"
+	AgentLatestVersion = "7.65.2"
 	// ClusterAgentLatestVersion corresponds to the latest stable cluster-agent release
-	ClusterAgentLatestVersion = "7.64.3"
+	ClusterAgentLatestVersion = "7.65.2"
 	// FIPSProxyLatestVersion corresponds to the latest stable fips-proxy release
 	FIPSProxyLatestVersion = "1.1.10"
 	// GCRContainerRegistry corresponds to the datadoghq GCR registry
