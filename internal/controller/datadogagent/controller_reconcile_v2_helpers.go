e663424115eea8e58bf8e4eae7df7103d62f0e69
khewonc <39867936+khewonc@users.noreply.github.com>
2025-05-14 17:43:13 -0400

    Add DCA token to DDA status

--- internal/controller/datadogagent/controller_reconcile_v2_helpers.go
+++ internal/controller/datadogagent/controller_reconcile_v2_helpers.go
@@ -213,8 +213,8 @@ func (r *Reconciler) applyAndCleanupDependencies(ctx context.Context, logger log
 
 // generateNewStatusFromDDA generates a new status from a DDA status.
 // If an existing DCA token is present, it is copied to the new status.
-func generateNewStatusFromDDA(ddaStatus *datadoghqv2alpha1.DatadogAgentStatus) datadoghqv2alpha1.DatadogAgentStatus {
-	status := datadoghqv2alpha1.DatadogAgentStatus{}
+func generateNewStatusFromDDA(ddaStatus *datadoghqv2alpha1.DatadogAgentStatus) *datadoghqv2alpha1.DatadogAgentStatus {
+	status := &datadoghqv2alpha1.DatadogAgentStatus{}
 	if ddaStatus != nil && ddaStatus.ClusterAgent != nil && ddaStatus.ClusterAgent.GeneratedToken != "" {
 		status.ClusterAgent = &datadoghqv2alpha1.DeploymentStatus{
 			GeneratedToken: ddaStatus.ClusterAgent.GeneratedToken,
