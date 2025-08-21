0f5ceb19321c37fd4f12dfae6bd7c7e64ec18148
Lénaïc Huard <lenaic.huard@datadoghq.com>
2025-08-21 22:58:29 +0200

    Add tests for `indentWriter`

--- internal/controller/datadogagent/feature/kubernetesstatecore/configmap.go
+++ internal/controller/datadogagent/feature/kubernetesstatecore/configmap.go
@@ -7,7 +7,6 @@ package kubernetesstatecore
 
 import (
 	"bytes"
-	"io"
 	"strconv"
 
 	"gopkg.in/yaml.v3"
@@ -40,51 +39,6 @@ func buildDefaultConfigMap(namespace, cmName string, content string) *corev1.Con
 		},
 	}
 	return configMap
-}
-
-// indentWriter wraps an io.Writer and adds indentation to each line
-type indentWriter struct {
-	w          io.Writer
-	indent     []byte
-	needIndent bool
-}
-
-// newIndentWriter creates a new indentWriter with the specified number of spaces for indentation
-func newIndentWriter(w io.Writer, spaces int) *indentWriter {
-	return &indentWriter{
-		w:          w,
-		indent:     bytes.Repeat([]byte(" "), spaces),
-		needIndent: true,
-	}
-}
-
-// Write implements io.Writer interface, adding indentation at the start of each line
-func (iw *indentWriter) Write(p []byte) (int, error) {
-	if len(p) == 0 {
-		return 0, nil
-	}
-
-	for _, b := range p {
-		if iw.needIndent && b != '\n' {
-			_, err := iw.w.Write(iw.indent)
-			if err != nil {
-				return 0, err
-			}
-			iw.needIndent = false
-		}
-
-		_, err := iw.w.Write([]byte{b})
-		if err != nil {
-			return 0, err
-		}
-
-		if b == '\n' {
-			iw.needIndent = true
-		}
-	}
-
-	// Return the original byte count to satisfy the io.Writer contract
-	return len(p), nil
 }
 
 // KSM should be configured as a cluster check only when there are Cluster Check
