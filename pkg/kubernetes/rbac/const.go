97a68e2d930cd6377ec9bd84a275c62bb19f84a0
Mark Spicer <mark.spicer@datadoghq.com>
2025-08-27 17:54:35 -0400

    rbac changes

--- pkg/kubernetes/rbac/const.go
+++ pkg/kubernetes/rbac/const.go
@@ -51,6 +51,7 @@ const (
 	DatadogMetricsStatusResource        = "datadogmetrics/status"
 	DatadogPodAutoscalersResource       = "datadogpodautoscalers"
 	DatadogPodAutoscalersStatusResource = "datadogpodautoscalers/status"
+	DatadogServiceMonitorsResource      = "datadogservicemonitors"
 	DeploymentsResource                 = "deployments"
 	EndpointsResource                   = "endpoints"
 	EndpointsSlicesResource             = "endpointslices"
