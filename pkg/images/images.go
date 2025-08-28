e7d3423a1d64c7a7b14fbfa48966a3445b0a8caa
Louis Coquerelle <louis.coquerelle@datadoghq.com>
2025-08-28 09:27:17 +0200

    [AGENTRUN-678] Update FIPS proxy version references to 1.1.16

--- pkg/images/images.go
+++ pkg/images/images.go
@@ -19,7 +19,7 @@ const (
 	// ClusterAgentLatestVersion corresponds to the latest stable cluster-agent release
 	ClusterAgentLatestVersion = "7.69.3"
 	// FIPSProxyLatestVersion corresponds to the latest stable fips-proxy release
-	FIPSProxyLatestVersion = "1.1.15"
+	FIPSProxyLatestVersion = "1.1.16"
 	// GCRContainerRegistry corresponds to the datadoghq GCR registry
 	GCRContainerRegistry = "gcr.io/datadoghq"
 	// DockerHubContainerRegistry corresponds to the datadoghq docker.io registry
