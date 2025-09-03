ef7580c9f52329aac13f4fa32b2fe7a6c5032c74
levan-m <116471169+levan-m@users.noreply.github.com>
2025-09-02 20:44:01 -0400

    Update cmd/main.go
    Co-authored-by: Sarah Wang <sarah.wang@datadoghq.com>

--- cmd/main.go
+++ cmd/main.go
@@ -224,7 +224,7 @@ func run(opts *options) error {
 	if opts.datadogAgentEnabled {
 		setupLog.Error(nil, "[WARNING] Upcoming Agent DaemonSet selector changes in Operator v1.20. If you rely on Datadog Agent pod labels e.g. in NetworkPolicies, verify if you may be impacted. See README for details.")
 		if opts.datadogAgentProfileEnabled {
-			setupLog.Error(nil, "[WARNING] Upcoming selector change in Agent DaemonSets managed by DAPs in Operator v1.18 and v1.20. If you rely on Datadog Agent pod labels e.g. in NetworkPolicies review if you may be impacted. See README for details.")
+			setupLog.Error(nil, "[WARNING] Upcoming selector changes in Agent DaemonSets managed by DAPs in Operator v1.18 and v1.20. If you rely on Datadog Agent pod labels, e.g. in NetworkPolicies, verify if you may be impacted. See README for details.")
 		}
 	}
 
