ad6dfd7667f01271a574f852a40be8c8146bc1e7
Fanny Jiang <fanny.jiang@datadoghq.com>
2025-09-09 15:31:34 -0400

    disable required DCA default

--- internal/controller/datadogagent/feature/enabledefault/feature.go
+++ internal/controller/datadogagent/feature/enabledefault/feature.go
@@ -62,10 +62,6 @@ func (f *defaultFeature) Configure(dda metav1.Object, ddaSpec *v2alpha1.DatadogA
 	}
 
 	return feature.RequiredComponents{
-		ClusterAgent: feature.RequiredComponent{
-			IsRequired: &trueValue,
-			Containers: []apicommon.AgentContainerName{apicommon.ClusterAgentContainerName},
-		},
 		Agent: feature.RequiredComponent{
 			IsRequired: &trueValue,
 			Containers: agentContainers,
