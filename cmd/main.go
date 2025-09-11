75bd104a7847d97ee246a2e34bad67653d96c79c
Lénaïc Huard <lenaic.huard@datadoghq.com>
2025-09-11 12:21:27 +0200

    Add update of role mappings in the `aws-auth` ConfigMap

--- cmd/main.go
+++ cmd/main.go
@@ -11,6 +11,10 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/cloudformation"
+	"github.com/aws/aws-sdk-go-v2/service/sts"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 
 	"helm.sh/helm/v3/pkg/action"
 	"helm.sh/helm/v3/pkg/cli"
@@ -43,6 +47,7 @@ func main() {
 	var (
 		clusterName        = flag.String("cluster-name", guessclusterName, "Name of the EKS cluster")
 		karpenterNamespace = flag.String("karpenter-namespace", "dd-karpenter", "Name of the Kubernetes namespace in which deploying Karpenter")
+		kubeconfig         = flag.String("kubeconfig", clientcmd.RecommendedHomeFile, "Path to the kubeconfig file")
 	)
 	flag.Parse()
 
@@ -60,6 +65,33 @@ func main() {
 
 	if err := aws.CreateOrUpdateStack(ctx, cloudformationClient, "dd-karpenter-"+*clusterName+"-podidentityrole", PodIdentityRoleCfn, map[string]string{
 		"ClusterName": *clusterName,
+	}); err != nil {
+		log.Fatal(err)
+	}
+
+	stsClient := sts.NewFromConfig(awsConfig)
+	callerIdentity, err := stsClient.GetCallerIdentity(ctx, &sts.GetCallerIdentityInput{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	if callerIdentity.Account == nil {
+		log.Fatal("unable to determine AWS account ID from STS GetCallerIdentity")
+	}
+	accountID := *callerIdentity.Account
+
+	k8sConfig, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+	k8sClientSet, err := kubernetes.NewForConfig(k8sConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := aws.EnsureAwsAuthRole(ctx, k8sClientSet, aws.RoleMapping{
+		RoleArn:  "arn:aws:iam::" + accountID + ":role/KarpenterNodeRole-" + *clusterName,
+		Username: "system:node:{{EC2PrivateDNSName}}",
+		Groups:   []string{"system:bootstrappers", "system:nodes"},
 	}); err != nil {
 		log.Fatal(err)
 	}
