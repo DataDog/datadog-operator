9f0c599ef73171dc4ffc98ef834f8efee494279b
Timothée Bavelier <timothee.bavelier@datadoghq.com>
2025-10-07 10:45:16 +0200

    Fix /debug/pprof endpoints

--- pkg/controller/debug/register.go
+++ pkg/controller/debug/register.go
@@ -31,10 +31,16 @@ func DefaultOptions() *Options {
 // GetExtraMetricHandlers creates debug endpoints.
 func GetExtraMetricHandlers() map[string]http.Handler {
 	handlers := make(map[string]http.Handler)
-	handlers["debug/pprof"] = http.HandlerFunc(pprof.Index)
-	handlers["/debug/pprof/cmdline"] = http.HandlerFunc(pprof.Index)
-	handlers["/debug/pprof/cmdline"] = http.HandlerFunc(pprof.Index)
-	handlers["/debug/pprof/symobol"] = http.HandlerFunc(pprof.Index)
-	handlers["/debug/pprof/trace"] = http.HandlerFunc(pprof.Index)
+	handlers["/debug/pprof/"] = http.HandlerFunc(pprof.Index)
+	handlers["/debug/pprof/cmdline"] = http.HandlerFunc(pprof.Cmdline)
+	handlers["/debug/pprof/profile"] = http.HandlerFunc(pprof.Profile)
+	handlers["/debug/pprof/symbol"] = http.HandlerFunc(pprof.Symbol)
+	handlers["/debug/pprof/trace"] = http.HandlerFunc(pprof.Trace)
+	handlers["/debug/pprof/heap"] = pprof.Handler("heap")
+	handlers["/debug/pprof/goroutine"] = pprof.Handler("goroutine")
+	handlers["/debug/pprof/threadcreate"] = pprof.Handler("threadcreate")
+	handlers["/debug/pprof/block"] = pprof.Handler("block")
+	handlers["/debug/pprof/mutex"] = pprof.Handler("mutex")
+	handlers["/debug/pprof/allocs"] = pprof.Handler("allocs")
 	return handlers
 }
