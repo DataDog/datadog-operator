77d7950e108888cd5ebf81afeccc816b8764e4d4
Sarah Wang <sarah.wang2009@gmail.com>
2025-10-22 16:14:58 -0400

    add basic scaffolding for helm metadata forwarder

--- cmd/main.go
+++ cmd/main.go
@@ -379,6 +379,7 @@ func run(opts *options) error {
 	}
 
 	setupAndStartOperatorMetadataForwarder(metadataLog, mgr.GetAPIReader(), versionInfo.String(), opts)
+	setupAndStartHelmMetadataForwarder(metadataLog, mgr.GetAPIReader(), versionInfo.String(), opts)
 
 	// +kubebuilder:scaffold:builder
 
@@ -507,4 +508,13 @@ func setupAndStartOperatorMetadataForwarder(logger logr.Logger, client client.Re
 	}
 
 	omf.Start()
+}
+
+func setupAndStartHelmMetadataForwarder(logger logr.Logger, client client.Reader, kubernetesVersion string, options *options) {
+	hmf := metadata.NewHelmMetadataForwarder(logger, client, kubernetesVersion, version.GetVersion())
+	hmf.HelmMetadata = metadata.HelmMetadata{
+		OperatorVersion:   version.GetVersion(),
+		KubernetesVersion: kubernetesVersion,
+	}
+	hmf.Start()
 }
