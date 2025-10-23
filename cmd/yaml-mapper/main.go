e3d61181d105ec77e0216df642972ded916ffd64
Fanny Jiang <fanny.jiang@datadoghq.com>
2025-10-23 18:54:20 -0400

    cleanup

--- cmd/yaml-mapper/main.go
+++ cmd/yaml-mapper/main.go
@@ -5,48 +5,6 @@
 
 package main
 
-//var (
-//	mappingFile string
-//	sourceFile  string
-//	destFile    string
-//	prefixFile  string
-//	ddaName     string
-//	namespace   string
-//	updateMap   bool
-//	printPtr    bool
-//)
-
-//const defaultDDAMappingPath = "mapping_datadog_helm_to_datadogagent_crd_v2.yaml"
-//
-//func main() {
-//	flags := pflag.NewFlagSet("yaml-mapper", pflag.ExitOnError)
-//	pflag.CommandLine = flags
-//
-//	yamlMapper := mapper.MapYaml
-//	flag.Usage = func() {
-//		fmt.Fprintf(os.Stderr, `yaml-mapper: migrate Datadog Helm values to the DatadogAgent CRD
-//Usage:
-//	helm-mapper -sourceFile=<FILE> -destFile=<DEST_FILE> -mappingFile=<MAPPING_FILE>
-//
-//Options:
-//`)
-//		flag.PrintDefaults()
-//	}
-//
-//	flag.StringVar(&mappingFile, "mappingFile", defaultDDAMappingPath, "Path to mapping YAML file. Example: mapping.yaml")
-//	flag.StringVar(&sourceFile, "sourceFile", "", "Path to source YAML file. Example: source.yaml")
-//	flag.StringVar(&destFile, "destFile", "destination.yaml", "Path to destination YAML file.")
-//	flag.StringVar(&prefixFile, "prefixFile", "", "Path to prefix YAML file. The content in this file will be prepended to the output.")
-//	flag.StringVar(&ddaName, "ddaName", "", "Name to use for the destination DDA manifest.")
-//	flag.StringVar(&namespace, "namespace", "", "Namespace to use in destination DDA manifest.")
-//	flag.BoolVar(&updateMap, "updateMap", false, fmt.Sprintf("Update 'mappingFile' with provided 'sourceFile'. (default false) If set to 'true', default mappingFile is %s and default sourceFile is latest published Datadog chart values.yaml.", defaultDDAMappingPath))
-//	flag.BoolVar(&printPtr, "printOutput", true, "print output to stdout")
-//
-//	flag.Parse()
-//
-//	mapper.MapYaml(mappingFile, sourceFile, destFile, prefixFile, ddaName, namespace, updateMap, printPtr)
-//}
-
 import (
 	"os"
 
