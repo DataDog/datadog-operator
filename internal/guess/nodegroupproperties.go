6b5f676e45bc406414265169a61006dfc6aff487
Lénaïc Huard <lenaic.huard@datadoghq.com>
2025-11-05 06:53:01 +0100

    Refine NodePools inference

--- internal/guess/nodegroupproperties.go
+++ internal/guess/nodegroupproperties.go
@@ -3,7 +3,6 @@ package guess
 import (
 	"context"
 	"fmt"
-	"strings"
 
 	"github.com/aws/aws-sdk-go-v2/service/eks"
 	ekstypes "github.com/aws/aws-sdk-go-v2/service/eks/types"
@@ -11,43 +10,8 @@ import (
 	corev1 "k8s.io/api/core/v1"
 )
 
-// NodeGroupProperties contains the characteristics of a specific EKS node group
-// required to create Karpenter resources (EC2NodeClass and NodePool)
-type NodeGroupProperties struct {
-	Name                      string
-	AMIID                     string
-	ReleaseVersion            string
-	Subnets                   []string
-	SecurityGroups            []string
-	InstanceTypes             []string
-	CapacityType              string
-	Labels                    map[string]string
-	Taints                    []corev1.Taint
-	NodeRoleARN               string
-	RemoteAccessSecurityGroup string
-	LaunchTemplate            *LaunchTemplateInfo
-	ScalingConfig             *ScalingConfig
-	DiskSize                  *int32
-}
-
-// LaunchTemplateInfo contains the Launch Template information
-type LaunchTemplateInfo struct {
-	ID      string
-	Name    string
-	Version string
-}
-
-// ScalingConfig contains the scaling configuration of the node group
-type ScalingConfig struct {
-	MinSize     *int32
-	MaxSize     *int32
-	DesiredSize *int32
-}
-
-// GetNodeGroupsProperties retrieves the properties of all node groups in an EKS cluster
-// and returns a list with the characteristics of each node group
-func GetNodeGroupsProperties(ctx context.Context, client *eks.Client, clusterName string) ([]NodeGroupProperties, error) {
-	nodeGroupsProperties := []NodeGroupProperties{}
+func GetNodeGroupsProperties(ctx context.Context, client *eks.Client, clusterName string) (*NodePoolsSet, error) {
+	nps := NewNodePoolsSet()
 
 	cluster, err := client.DescribeCluster(ctx, &eks.DescribeClusterInput{
 		Name: &clusterName,
@@ -67,103 +31,38 @@ func GetNodeGroupsProperties(ctx context.Context, client *eks.Client, clusterNam
 		}
 
 		for _, ngName := range nodegroupsList.Nodegroups {
-			properties, err := extractNodeGroupProperties(ctx, client, clusterName, ngName)
+			nodegroup, err := client.DescribeNodegroup(ctx, &eks.DescribeNodegroupInput{
+				ClusterName:   &clusterName,
+				NodegroupName: &ngName,
+			})
 			if err != nil {
-				return nil, fmt.Errorf("failed to extract properties from node group %s: %w", ngName, err)
+				return nil, fmt.Errorf("failed to describe node group %s: %w", ngName, err)
 			}
+
+			ng := nodegroup.Nodegroup
+			if ng == nil {
+				return nil, fmt.Errorf("node group %s not found", ngName)
+			}
+
+			params := NodePoolsSetAddParams{
+				AMIID:     string(ng.AmiType),
+				SubnetIDs: ng.Subnets,
+				Labels:    ng.Labels,
+				Taints:    lo.Map(ng.Taints, func(t ekstypes.Taint, _ int) corev1.Taint { return convertTaint(t) }),
+			}
+
 			if cluster.Cluster != nil && cluster.Cluster.ResourcesVpcConfig != nil && cluster.Cluster.ResourcesVpcConfig.VpcId != nil {
-				properties.SecurityGroups = append(properties.SecurityGroups, *cluster.Cluster.ResourcesVpcConfig.ClusterSecurityGroupId)
+				params.SecurityGroupIDs = []string{*cluster.Cluster.ResourcesVpcConfig.ClusterSecurityGroupId}
 			}
-			nodeGroupsProperties = append(nodeGroupsProperties, properties)
+
+			nps.Add(params)
 		}
 
 		nextToken = nodegroupsList.NextToken
 		if nextToken == nil {
-			return nodeGroupsProperties, nil
+			return nps, nil
 		}
 	}
-}
-
-// extractNodeGroupProperties extracts the properties of a specific node group
-func extractNodeGroupProperties(ctx context.Context, client *eks.Client, clusterName, nodeGroupName string) (NodeGroupProperties, error) {
-	nodegroup, err := client.DescribeNodegroup(ctx, &eks.DescribeNodegroupInput{
-		ClusterName:   &clusterName,
-		NodegroupName: &nodeGroupName,
-	})
-	if err != nil {
-		return NodeGroupProperties{}, fmt.Errorf("failed to describe node group %s: %w", nodeGroupName, err)
-	}
-
-	ng := nodegroup.Nodegroup
-	if ng == nil {
-		return NodeGroupProperties{}, fmt.Errorf("node group %s not found", nodeGroupName)
-	}
-
-	properties := NodeGroupProperties{
-		Name:           *ng.NodegroupName,
-		Subnets:        ng.Subnets,
-		InstanceTypes:  ng.InstanceTypes,
-		Labels:         ng.Labels,
-		Taints:         lo.Map(ng.Taints, func(t ekstypes.Taint, _ int) corev1.Taint { return convertTaint(t) }),
-		SecurityGroups: []string{},
-	}
-
-	for l := range properties.Labels {
-		if strings.Contains(l, "eksctl.io") {
-			delete(properties.Labels, l)
-		}
-	}
-
-	if ng.ReleaseVersion != nil {
-		properties.ReleaseVersion = *ng.ReleaseVersion
-	}
-
-	if ng.CapacityType != "" {
-		properties.CapacityType = string(ng.CapacityType)
-	}
-
-	if ng.NodeRole != nil {
-		properties.NodeRoleARN = *ng.NodeRole
-	}
-
-	if ng.DiskSize != nil {
-		properties.DiskSize = ng.DiskSize
-	}
-
-	if ng.LaunchTemplate != nil {
-		properties.LaunchTemplate = &LaunchTemplateInfo{}
-		if ng.LaunchTemplate.Id != nil {
-			properties.LaunchTemplate.ID = *ng.LaunchTemplate.Id
-		}
-		if ng.LaunchTemplate.Name != nil {
-			properties.LaunchTemplate.Name = *ng.LaunchTemplate.Name
-		}
-		if ng.LaunchTemplate.Version != nil {
-			properties.LaunchTemplate.Version = *ng.LaunchTemplate.Version
-		}
-		// Note: To get the AMI ID from the Launch Template, an additional
-		// EC2 DescribeLaunchTemplateVersions call would be required
-	}
-
-	// Extract scaling configuration
-	if ng.ScalingConfig != nil {
-		properties.ScalingConfig = &ScalingConfig{
-			MinSize:     ng.ScalingConfig.MinSize,
-			MaxSize:     ng.ScalingConfig.MaxSize,
-			DesiredSize: ng.ScalingConfig.DesiredSize,
-		}
-	}
-
-	// Extract security groups
-	if ng.Resources != nil {
-		if ng.Resources.RemoteAccessSecurityGroup != nil {
-			properties.RemoteAccessSecurityGroup = *ng.Resources.RemoteAccessSecurityGroup
-			properties.SecurityGroups = append(properties.SecurityGroups, *ng.Resources.RemoteAccessSecurityGroup)
-		}
-		// Note: There may be other security groups defined in the Launch Template
-	}
-
-	return properties, nil
 }
 
 func convertTaint(in ekstypes.Taint) (out corev1.Taint) {
--- internal/k8s/ec2nodeclass.go
+++ internal/k8s/ec2nodeclass.go
@@ -7,36 +7,36 @@ import (
 	"github.com/samber/lo"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/L3n41c/karpenter_installer_wizard/internal/guess"
 )
 
-func CreateOrUpdateEC2NodeClass(ctx context.Context, client client.Client, name, clusterName string, AMIs, subnets, sgs []string) error {
-	ec2NodeClass := &karpawsv1.EC2NodeClass{
+func CreateOrUpdateEC2NodeClass(ctx context.Context, client client.Client, clusterName string, nc guess.EC2NodeClass) error {
+	return createOrUpdate(ctx, client, &karpawsv1.EC2NodeClass{
 		TypeMeta: metav1.TypeMeta{
 			APIVersion: "karpenter.k8s.aws/v1",
 			Kind:       "EC2NodeClass",
 		},
 		ObjectMeta: metav1.ObjectMeta{
-			Name: name,
+			Name: nc.Name,
 		},
 		Spec: karpawsv1.EC2NodeClassSpec{
 			Role: "KarpenterNodeRole-" + clusterName,
-			AMISelectorTerms: lo.Map(AMIs, func(ami string, _ int) karpawsv1.AMISelectorTerm {
+			AMISelectorTerms: lo.Map(nc.AMIIDs, func(ami string, _ int) karpawsv1.AMISelectorTerm {
 				return karpawsv1.AMISelectorTerm{
 					Alias: "al2023@latest", // TODO: FIX
 				}
 			}),
-			SubnetSelectorTerms: lo.Map(subnets, func(subnet string, _ int) karpawsv1.SubnetSelectorTerm {
+			SubnetSelectorTerms: lo.Map(nc.SubnetIDs, func(subnetID string, _ int) karpawsv1.SubnetSelectorTerm {
 				return karpawsv1.SubnetSelectorTerm{
-					ID: subnet,
+					ID: subnetID,
 				}
 			}),
-			SecurityGroupSelectorTerms: lo.Map(sgs, func(sg string, _ int) karpawsv1.SecurityGroupSelectorTerm {
+			SecurityGroupSelectorTerms: lo.Map(nc.SecurityGroupIDs, func(sgID string, _ int) karpawsv1.SecurityGroupSelectorTerm {
 				return karpawsv1.SecurityGroupSelectorTerm{
-					ID: sg,
+					ID: sgID,
 				}
 			}),
 		},
-	}
-
-	return createOrUpdate(ctx, client, ec2NodeClass)
+	})
 }
