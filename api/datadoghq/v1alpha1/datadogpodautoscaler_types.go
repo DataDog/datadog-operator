0e379189d530a45928ea15d63cf098fe7685e638
Nick Birnberg <nick.birnberg@datadoghq.com>
2025-12-08 13:42:58 -0500

    Remove v1alpha1 changes

--- api/datadoghq/v1alpha1/datadogpodautoscaler_types.go
+++ api/datadoghq/v1alpha1/datadogpodautoscaler_types.go
@@ -62,8 +62,6 @@ import (
 //         limits:
 //           minAllowed:
 //           maxAllowed:
-//   recommendationOptions:
-//     oomBumpUpRatio: 1.2
 
 // DatadogPodAutoscalerSpec defines the desired state of DatadogPodAutoscaler
 type DatadogPodAutoscalerSpec struct {
@@ -91,10 +89,6 @@ type DatadogPodAutoscalerSpec struct {
 
 	// Constraints defines constraints that should always be respected.
 	Constraints *common.DatadogPodAutoscalerConstraints `json:"constraints,omitempty"`
-
-	// RecommendationOptions defines options for how recommendations are generated.
-	// +optional
-	RecommendationOptions *common.DatadogPodAutoscalerRecommendationOptions `json:"recommendationOptions,omitempty"`
 }
 
 // DatadogPodAutoscalerOwner defines the source of truth for this object (local or remote)
