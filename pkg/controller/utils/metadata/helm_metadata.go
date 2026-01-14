79a443f5c49e04d8db631a4f844e747b8ebda515
Sarah Wang <sarah.wang2009@gmail.com>
2026-01-14 16:13:37 -0500

    change chart name

--- pkg/controller/utils/metadata/helm_metadata.go
+++ pkg/controller/utils/metadata/helm_metadata.go
@@ -41,9 +41,9 @@ const (
 var (
 	versionRegexp = regexp.MustCompile(`\.v(\d+)$`)
 	allowedCharts = map[string]bool{
-		"datadog":                     true,
-		"datadog-operator":            true,
-		"datadog-agent-with-operator": true,
+		"datadog":          true,
+		"datadog-operator": true,
+		"datadog-agent":    true,
 	}
 )
 
