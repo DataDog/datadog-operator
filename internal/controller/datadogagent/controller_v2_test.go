5dfcc10c758eff53b8d27301dd65e04014a6e21b
Sarah Wang <sarah.wang2009@gmail.com>
2026-01-23 16:27:09 -0500

    add controller test for EKS nodes with different labels

--- internal/controller/datadogagent/controller_v2_test.go
+++ internal/controller/datadogagent/controller_v2_test.go
@@ -1220,6 +1220,91 @@ func Test_Control_Plane_Monitoring(t *testing.T) {
 			},
 		},
 		{
+			name: "[introspection] Control Plane Monitoring with EKS multi-node (Fargate + Standard + Bottlerocket)",
+			fields: fields{
+				scheme:   s,
+				recorder: recorder,
+			},
+			loadFunc: func(c client.Client) *v2alpha1.DatadogAgent {
+				return createDatadogAgentWithClusterChecks(c, resourcesNamespace, resourcesName)
+			},
+			nodes: []client.Object{
+				&corev1.Node{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "eks-fargate-node",
+						Labels: map[string]string{
+							"eks.amazonaws.com/compute-type":    "fargate",
+							"eks.amazonaws.com/fargate-profile": "my-profile",
+						},
+					},
+				},
+				&corev1.Node{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "eks-standard-node",
+						Labels: map[string]string{
+							kubernetes.EKSProviderLabel: "ami-0e7f88829f3d06e29",
+							"eks.amazonaws.com/nodegroup": "standard-nodes",
+						},
+					},
+				},
+				&corev1.Node{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "eks-bottlerocket-node",
+						Labels: map[string]string{
+							kubernetes.EKSProviderLabel: "ami-0fa9d45aa38272f15",
+							"eks.amazonaws.com/nodegroup": "bottlerocket-nodes",
+						},
+					},
+				},
+			},
+			want:    reconcile.Result{RequeueAfter: defaultRequeueDuration},
+			wantErr: false,
+			wantFunc: func(t *testing.T, c client.Client) error {
+				// All EKS nodes should be detected as single "eks" provider
+				if err := verifyDCADeployment(t, c, resourcesName, resourcesNamespace, dcaName, "eks"); err != nil {
+					return err
+				}
+				// Should create single DaemonSet for all EKS node types
+				expectedDaemonsets := []string{
+					dsName,
+				}
+				return verifyDaemonsetNames(t, c, resourcesNamespace, expectedDaemonsets)
+			},
+		},
+		{
+			name: "[introspection] Control Plane Monitoring with EKS eksctl-provisioned cluster",
+			fields: fields{
+				scheme:   s,
+				recorder: recorder,
+			},
+			loadFunc: func(c client.Client) *v2alpha1.DatadogAgent {
+				return createDatadogAgentWithClusterChecks(c, resourcesNamespace, resourcesName)
+			},
+			nodes: []client.Object{
+				&corev1.Node{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "eks-eksctl-node",
+						Labels: map[string]string{
+							"alpha.eksctl.io/cluster-name":   "my-cluster",
+							"alpha.eksctl.io/nodegroup-name": "my-nodegroup",
+						},
+					},
+				},
+			},
+			want:    reconcile.Result{RequeueAfter: defaultRequeueDuration},
+			wantErr: false,
+			wantFunc: func(t *testing.T, c client.Client) error {
+				// Eksctl labels should also be detected as EKS
+				if err := verifyDCADeployment(t, c, resourcesName, resourcesNamespace, dcaName, "eks"); err != nil {
+					return err
+				}
+				expectedDaemonsets := []string{
+					dsName,
+				}
+				return verifyDaemonsetNames(t, c, resourcesNamespace, expectedDaemonsets)
+			},
+		},
+		{
 			name: "[introspection] Control Plane Monitoring with multiple providers",
 			fields: fields{
 				scheme:   s,
