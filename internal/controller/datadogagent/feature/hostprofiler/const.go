2aa9656e05bdf6386fa5765ac9acb6ce07698834
Timothée Bavelier <timothee.bavelier@datadoghq.com>
2026-03-24 14:34:49 +0100

    run make fmt

--- internal/controller/datadogagent/feature/hostprofiler/const.go
+++ internal/controller/datadogagent/feature/hostprofiler/const.go
@@ -4,5 +4,5 @@ const (
 	hostProfilerVolumeName     = "host-profiler-config-volume"
 	hostProfilerConfigFileName = "host-profiler-config.yaml"
 	// DefaultHostProfilerConf default otel agent ConfigMap name
-	defaultHostProfilerConf         = "host-profiler-config"
+	defaultHostProfilerConf = "host-profiler-config"
 )
