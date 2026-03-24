8476af02ec2ad6360610be73f5b6698d4d55603a
Mathew Estafanous <mathew.estafanous@datadoghq.com>
2026-03-24 16:15:38 -0400

    enhance: add kubernetese use endpoint slices global with default true

--- api/datadoghq/v2alpha1/zz_generated.deepcopy.go
+++ api/datadoghq/v2alpha1/zz_generated.deepcopy.go
@@ -2052,6 +2052,11 @@ func (in *GlobalConfig) DeepCopyInto(out *GlobalConfig) {
 			(*out)[key] = outVal
 		}
 	}
+	if in.KubernetesUseEndpointSlices != nil {
+		in, out := &in.KubernetesUseEndpointSlices, &out.KubernetesUseEndpointSlices
+		*out = new(bool)
+		**out = **in
+	}
 	if in.CSI != nil {
 		in, out := &in.CSI, &out.CSI
 		*out = new(CSIConfig)
