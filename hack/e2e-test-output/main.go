6d043768d7252c1f5d018e474215f27377df41aa
Timothée Bavelier <timothee.bavelier@datadoghq.com>
2026-06-03 11:26:28 +0200

    add test for hack function + do not have global cache to avoid polluting image jobs + safety measure for dockerignore

--- hack/e2e-test-output/main.go
+++ hack/e2e-test-output/main.go
@@ -12,24 +12,30 @@ type testEvent struct {
 }
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
+	os.Exit(run(os.Stdin, os.Stdout, os.Stderr))
+}
+
+func run(input io.Reader, output, errorOutput io.Writer) int {
+	scanner := bufio.NewScanner(input)
 	scanner.Buffer(make([]byte, 64*1024), 10*1024*1024)
 
 	for scanner.Scan() {
 		var event testEvent
 		line := scanner.Bytes()
 		if err := json.Unmarshal(line, &event); err != nil {
-			_, _ = os.Stdout.Write(line)
-			_, _ = os.Stdout.Write([]byte("\n"))
+			_, _ = output.Write(line)
+			_, _ = output.Write([]byte("\n"))
 			continue
 		}
 		if event.Output != "" {
-			_, _ = io.WriteString(os.Stdout, event.Output)
+			_, _ = io.WriteString(output, event.Output)
 		}
 	}
 
 	if err := scanner.Err(); err != nil {
-		_, _ = io.WriteString(os.Stderr, "reading go test output: "+err.Error()+"\n")
-		os.Exit(1)
+		_, _ = io.WriteString(errorOutput, "reading go test output: "+err.Error()+"\n")
+		return 1
 	}
+
+	return 0
 }
