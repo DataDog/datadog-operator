44b94e7fe4fda551609008c7ace678964492b100
khewonc <39867936+khewonc@users.noreply.github.com>
2026-06-17 13:17:43 -0400

    Update agent image to 7.80.2 (#3155)
    (cherry picked from commit 92788bfa52fdf75f9ab0df090b76cbb23a0e1eb3)

--- pkg/images/images.go
+++ pkg/images/images.go
@@ -16,11 +16,11 @@ import (
 
 const (
 	// AgentLatestVersion corresponds to the latest stable agent release
-	AgentLatestVersion = "7.80.1"
+	AgentLatestVersion = "7.80.2"
 	// ClusterAgentLatestVersion corresponds to the latest stable cluster-agent release
-	ClusterAgentLatestVersion = "7.80.1"
+	ClusterAgentLatestVersion = "7.80.2"
 	// DdotCollectorLatestVersion corresponds to the latest stable ddot-collector release
-	DdotCollectorLatestVersion = "7.80.1"
+	DdotCollectorLatestVersion = "7.80.2"
 	// FIPSProxyLatestVersion corresponds to the latest stable fips-proxy release
 	FIPSProxyLatestVersion = "1.1.26"
 	// DDOTFIPSMinimumVersion is the minimum version at which ddot-collector publishes a -fips variant.
