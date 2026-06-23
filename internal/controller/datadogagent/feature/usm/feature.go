eb81e88cea64f21c89d50ad932f81b415d3d07bb
Timothée Bavelier <timothee.bavelier@datadoghq.com>
2026-06-23 16:01:19 +0200

    Do not set process-agent AppArmor for NPM fallback

--- internal/controller/datadogagent/feature/usm/feature.go
+++ internal/controller/datadogagent/feature/usm/feature.go
@@ -126,9 +126,6 @@ func (f *usmFeature) ManageNodeAgent(managers feature.PodTemplateManagers) error
 
 	// annotations
 	managers.Annotation().AddAnnotation(common.SystemProbeAppArmorAnnotationKey, common.SystemProbeAppArmorAnnotationValue)
-	if !f.directSend {
-		managers.Annotation().AddAnnotation(common.AppArmorAnnotationKey+"/"+string(apicommon.ProcessAgentContainerName), "unconfined")
-	}
 
 	// security context capabilities
 	managers.SecurityContext().AddCapabilitiesToContainer(agent.DefaultCapabilitiesForSystemProbe(), apicommon.SystemProbeContainerName)
