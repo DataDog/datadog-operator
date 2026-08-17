51871d059d0f61c06dd77ebf9f6831ca49a25270
39867936+khewonc <39867936+khewonc@users.noreply.github.com>
2026-08-17 16:42:10 -0400

    remove unused functions since #1286

--- internal/controller/datadogagentinternal/utils.go
+++ internal/controller/datadogagentinternal/utils.go
@@ -16,11 +16,9 @@ import (
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
-	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
 
 	apicommon "github.com/DataDog/datadog-operator/api/datadoghq/common"
 	"github.com/DataDog/datadog-operator/pkg/kubernetes"
@@ -51,44 +49,6 @@ func mergeAnnotationsLabels(ctx context.Context, previousVal map[string]string, 
 	return mergedMap
 }
 
-// CheckOwnerReference return true if owner is the owner of the object
-func CheckOwnerReference(owner, object metav1.Object) bool {
-	return metav1.IsControlledBy(object, owner)
-}
-
-// SetOwnerReference sets owner as a OwnerReference.
-func SetOwnerReference(owner, object metav1.Object, scheme *runtime.Scheme) error {
-	ro, ok := owner.(runtime.Object)
-	if !ok {
-		return fmt.Errorf("%T is not a runtime.Object, cannot call SetControllerReference", owner)
-	}
-
-	gvk, err := apiutil.GVKForObject(ro, scheme)
-	if err != nil {
-		return err
-	}
-
-	// Create a new ref
-	ref := *newOwnerRef(owner, schema.GroupVersionKind{Group: gvk.Group, Version: gvk.Version, Kind: gvk.Kind})
-
-	existingRefs := object.GetOwnerReferences()
-	fi := -1
-	for i, r := range existingRefs {
-		if referSameObject(ref, r) {
-			fi = i
-		}
-	}
-	if fi == -1 {
-		existingRefs = append(existingRefs, ref)
-	} else {
-		existingRefs[fi] = ref
-	}
-
-	// Update owner references
-	object.SetOwnerReferences(existingRefs)
-	return nil
-}
-
 // newOwnerRef creates an OwnerReference pointing to the given owner.
 func newOwnerRef(owner metav1.Object, gvk schema.GroupVersionKind) *metav1.OwnerReference {
 	blockOwnerDeletion := true
@@ -101,21 +61,6 @@ func newOwnerRef(owner metav1.Object, gvk schema.GroupVersionKind) *metav1.Owner
 		BlockOwnerDeletion: &blockOwnerDeletion,
 		Controller:         &isController,
 	}
-}
-
-// Returns true if a and b point to the same object
-func referSameObject(a, b metav1.OwnerReference) bool {
-	aGV, err := schema.ParseGroupVersion(a.APIVersion)
-	if err != nil {
-		return false
-	}
-
-	bGV, err := schema.ParseGroupVersion(b.APIVersion)
-	if err != nil {
-		return false
-	}
-
-	return aGV == bGV && a.Kind == b.Kind && a.Name == b.Name
 }
 
 // createOwnerReferencePatch creates a patch from owner references
