977fa8fad167f15a9df700c9ed4f59887fb0d82b
Shuhei Kitagawa <shuhei.kitagawa@datadoghq.com>
2026-09-21 16:24:46 +0200

    Add DatadogBYOCCluster API and CRD

--- hack/patch-crds.sh
+++ hack/patch-crds.sh
@@ -14,6 +14,9 @@ v1=config/crd/bases/v1
 # Remove defaultOverride section in DatadogAgent status due to the error: "datadoghq.com_datadogagents.yaml bigger than total allowed limit"
 $YQ -i 'del(.spec.versions[].schema.openAPIV3Schema.properties.status.properties.defaultOverride)' "$ROOT/$v1/datadoghq.com_datadogagents.yaml"
 
+# Remove OpenAPI schema descriptions from the BYOC CRD to keep its size under control.
+$YQ -i '(.spec.versions[].schema.openAPIV3Schema) |= del(.. | .description?)' "$ROOT/$v1/datadoghq.com_datadogbyocclusters.yaml"
+
 for crd in "$ROOT/$v1"/*.yaml
 do
   $YQ -i -P "$crd"
